1d3fefbe5f81648441646861ea43780095298183
sunghoon <sunghoon@bharvest.io>
2021-03-30 17:59:29 +0900

    chore: add reset css

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import "./assets/scss/index.scss"
 
 createApp(App)
   .use(store)
