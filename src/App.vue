99276f19cbf9900bf1795d764aff2fbb2c9b4376
sunghoon <sunghoon@bharvest.io>
2021-04-09 17:06:43 +0900

    feat: add starport layout

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@
 </template>
 <script lang="ts">
 import { defineComponent } from 'vue';
+import '@starport/vue/lib/starport-vue.css';
 export default defineComponent({
   name: 'App',
 });
---
+++ src/components/layout/SideBar.vue
@@ -0,0 +1,31 @@
+<template>
+  <SpSidebar @sidebar-open="sidebarOpen = true" @sidebar-close="sidebarOpen = false">
+    <template #default>
+      <SpLinkIcon link="/" text="Dashboard" icon="Dashboard" />
+      <SpLinkIcon link="/pool" text="Pool" icon="Form" />
+      <SpLinkIcon link="/swap" text="Swap" icon="Form" />
+    </template>
+    <template #footer>
+      <SpStatusAPI :show-text="sidebarOpen" />
+      <SpStatusRPC :show-text="sidebarOpen" />
+      <SpStatusWS :show-text="sidebarOpen" />
+      <div class="sp-text">Build: v0.3.8</div>
+    </template>
+  </SpSidebar>
+</template>
+
+<script>
+export default {
+  name: 'Sidebar',
+  data() {
+    return {
+      sidebarOpen: true,
+    };
+  },
+  computed: {
+    hasWallet() {
+      return this.$store.hasModule(['common', 'wallet']);
+    },
+  },
+};
+</script>
