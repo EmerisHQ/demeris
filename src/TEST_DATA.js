1f751f4df8570a890853c1f7f9be215afc91d1de
sunghoon <sunghoon@bharvest.io>
2021-04-12 16:57:28 +0900

    chore: add test data

---
+++ src/TEST_DATA.js
@@ -0,0 +1,6 @@
+export const TEST_DATA = {
+    wallets: {
+        ATOM: { amount: 1250, denom: 'ATOM', source: 'Cosmos Hub' },
+        KAVA: { amount: 800, denom: 'KAVA', source: 'Kava' },
+    }
+}
