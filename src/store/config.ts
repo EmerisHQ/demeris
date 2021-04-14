6027c879f4bda225029a0c865597d0ee233b746e
Clockwork <alex@clockwork.gr>
2021-04-14 22:48:10 +0300

    feat: Add liquidity module, vuex config, build fix

--- src/store/config.ts
+++ src/store/config.ts
@@ -1,8 +1,12 @@
 import { env, blocks, wallet,transfers,relayers } from '@starport/vuex'
 import { Store } from 'vuex'
 import { NavigatorState } from './index'
+import liquidityModules from '@starport/tendermint-liquidity-js/index'
 
 export default function init(store:Store<NavigatorState>):void {
+	for (const moduleInit of Object.values(liquidityModules)) {
+		moduleInit(store)
+	}
 	transfers(store)
 	blocks(store)
 	env(store)
