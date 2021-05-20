c0dfa2e45ddefc430280d6cd472c7ba1ba07c5af
sunghoon <sunghoon@bharvest.io>
2021-05-21 01:57:46 +0900

    feat: apply dynamic class for button status & add props for generic use of a button

--- src/components/ui/Button.vue
+++ src/components/ui/Button.vue
@@ -1,30 +1,20 @@
 <template>
   <div>
     <!-- Basic button implementation. At minimum primary/secondary types, busy and disabled states, can be a link,router_link or trigger a custom clickHandler //-->
-    <button @click="swap" class="button s-0 w-medium elevation-button">Swap</button>
+    <button @click="clickFunction" :class="status" class="button s-0 w-medium elevation-button">
+      {{ name }}
+    </button>
   </div>
 </template>
 <script lang="ts">
 import { defineComponent } from 'vue';
-import useButton from '@/setups/Button.vue';
+
 export default defineComponent({
   name: 'Button',
-
-  setup() {
-    const { buttonFunction } = useButton();
-
-    function swap() {
-      buttonFunction({
-        type: 'link-current-page',
-        function: function test() {
-          alert('test');
-        },
-      });
-    }
-
-    return {
-      swap,
-    };
+  props: {
+    name: { type: String, required: true },
+    status: { type: String, required: false, default: 'normal' },
+    clickFunction: { type: Function, required: false, default: null },
   },
 });
 </script>
@@ -32,12 +22,24 @@ export default defineComponent({
 .button {
   width: 100%;
   padding: 1.6rem 0;
-  background-color: var(--text);
   color: var(--bg);
 
   border-radius: 8px;
   border: none;
   outline: none;
   cursor: pointer;
+}
+
+.normal {
+  background-color: var(--text);
+}
+
+.muted {
+  background-color: var(--muted);
+}
+
+.inactive {
+  background-color: var(--inactive);
+  pointer-events: none;
 }
 </style>
