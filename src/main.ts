f3a0f1721b4d49e41e8dea87c92caab2c5e5dd3c
Lúcio Rubens <4539235+luciorubeens@users.noreply.github.com>
2021-05-20 09:58:16 -0300

    feat: add `AssetChainsIndicator` component (#37)
    Co-authored-by: Lucio Rubens <luciorubeens@users.noreply.github.com>

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import './assets/scss/index.scss';
+import 'tippy.js/dist/tippy.css';
 
 import vueLib from '@starport/vue';
 import { createApp } from 'vue';
+import VueTippy from 'vue-tippy';
 
 import App from './App.vue';
 import router from './router';
@@ -13,4 +15,5 @@ app
   .use(store)
   .use(router)
   .use(vueLib)
+  .use(VueTippy)
   .mount('#app');
