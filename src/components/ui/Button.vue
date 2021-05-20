d96270ea27734a91cb9fb7bf40ae5bd505f99249
sunghoon <sunghoon@bharvest.io>
2021-05-20 10:47:50 +0900

    WIP: action button

--- src/components/ui/Button.vue
+++ src/components/ui/Button.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <!-- Basic button implementation. At minimum primary/secondary types, busy and disabled states, can be a link,router_link or trigger a custom clickHandler //-->
+    <button class="button s-0">Swap</button>
   </div>
 </template>
 <script lang="ts">
@@ -9,3 +10,16 @@ export default defineComponent({
   name: 'Button'
 })
 </script>
+<style lang="scss" scoped>
+.button {
+  width: 100%;
+  padding: 1.6rem 0;
+  background-color: var(--text);
+  color:var(--bg);
+
+  border-radius: 8px;
+  border: none;
+  outline:none;
+  cursor: pointer;
+}
+</style>
