b3514bd24fa4526139bafd97275933d2731c4137
sunghoon <sunghoon@bharvest.io>
2021-05-25 04:58:31 +0900

    feat: add coinName filter

--- src/filters/string.ts
+++ src/filters/string.ts
@@ -1,8 +1,9 @@
 export default {
     toUpperCase: (value: string) => {
-        {
-            if (!value) return ''
-            return value.toUpperCase()
-        }
+        if (!value) return ''
+        return value.toUpperCase()
+    },
+    getCoinName: (value: string) => {
+        return value.substr(1).toUpperCase()
     }
 }
