10f3601db50a3983e5cadef4b2b088c8a557f45e
sunghoon <sunghoon@bharvest.io>
2021-06-02 15:44:31 +0900

    WIP: focus border

--- src/components/common/Search.vue
+++ src/components/common/Search.vue
@@ -1,7 +1,8 @@
 <template>
-  <div class="search">
+  <div class="search" :style="isFocused ? '' : 'background-image: none'" @click="setFocus">
     <Icon :name="'MagnifyingGlassIcon'" :icon-size="1.6" />
     <input
+      ref="searchInput"
       :value="keyword"
       class="s-0 w-normal"
       type="text"
@@ -11,7 +12,7 @@
   </div>
 </template>
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue';
 
 import Icon from '@/components/ui/Icon.vue';
 export default defineComponent({
@@ -21,25 +22,52 @@ export default defineComponent({
     keyword: { type: String, required: false, default: null },
   },
   emits: ['update:keyword'],
+  setup() {
+    const searchInput = ref(null);
+    const isFocused = ref(false);
+
+    function setFocus() {
+      isFocused.value = true;
+      searchInput.value.focus();
+    }
+
+    return {
+      searchInput,
+      setFocus,
+      isFocused,
+    };
+  },
 });
 </script>
 <style lang="scss" scoped>
 .search {
   display: flex;
-  padding: 1.15rem 1.2rem;
   border-radius: 10px;
   background-color: var(--fg-trans);
+
+  border: 2px solid transparent;
+
+  background-image: linear-gradient(white, white), linear-gradient(to right, rgba(255, 222, 157, 1), #9ff9ff);
+  background-origin: border-box;
+  background-clip: content-box, border-box;
+
+  cursor: text;
 
   input {
     width: 100%;
     margin-left: 12px;
     outline: none;
-
+    margin: 1.15rem 1.2rem;
     background-color: transparent;
+    border: 2px solid transparent;
   }
 
   input::placeholder {
     color: var(--inactive);
+  }
+
+  .icon {
+    margin-left: 1.2rem;
   }
 }
 </style>
