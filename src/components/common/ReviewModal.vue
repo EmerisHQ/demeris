c5b95da61a4d4eea860b6f0f66f0b918f0e8f92a
sunghoon <sunghoon@bharvest.io>
2021-06-07 03:56:24 +0900

    WIP: review amount info

--- src/components/common/ReviewModal.vue
+++ src/components/common/ReviewModal.vue
@@ -1,6 +1,32 @@
 <template>
   <div class="denom-select-modal-wrapper elevation-panel">
     <GobackWithClose @goback="emitHandler('goback')" @close="emitHandler('close')" />
+    <div class="title s-2 w-bold">Review your swap details</div>
+    <div class="amount-info">
+      <div class="amount-info__type s-minus w-bold">Pay</div>
+      <div class="amount-info__detail">
+        <div class="amount-info__detail__coin">
+          <img class="amount-info__detail__coin-image" :src="require(`@/assets/coins/atom.png`)" alt="pay coin" />
+          <div class="amount-info__detail__coin-amount s-0 w-medium">500.2</div>
+          <div class="amount-info__detail__coin-denom s-0 w-medium">ATOM</div>
+        </div>
+        <div class="amount-info__detail-chain s-minus">Cosmos Hub</div>
+      </div>
+    </div>
+    <div class="amount-info">
+      <div class="amount-info__type s-minus w-bold">
+        Receive
+        <div class="amount-info__type-subtitle w-normal">(estimated)</div>
+      </div>
+      <div class="amount-info__detail">
+        <div class="amount-info__detail__coin">
+          <img class="amount-info__detail__coin-image" :src="require(`@/assets/coins/luna.png`)" alt="receive coin" />
+          <div class="amount-info__detail__coin-amount s-0 w-medium">500.2</div>
+          <div class="amount-info__detail__coin-denom s-0 w-medium">ATOM</div>
+        </div>
+        <div class="amount-info__detail-chain s-minus">Cosmos Hub</div>
+      </div>
+    </div>
   </div>
 </template>
 <script lang="ts">
@@ -38,21 +64,40 @@ export default defineComponent({
   background-color: var(--surface);
   z-index: 10;
 
-  .search-bar {
+  .title {
     padding: 0 2.4rem 2.4rem;
+    text-align: left;
   }
 
-  .coin-list {
-    padding: 0 1.6rem 0 2.4rem;
-    height: 37.8rem;
+  .amount-info {
+    display: flex;
+    justify-content: space-between;
 
-    overflow-y: scroll;
+    color: var(--text);
 
-    -ms-overflow-style: none; /* IE and Edge */
-    scrollbar-width: none; /* Firefox */
-
-    &::-webkit-scrollbar {
-      display: none; /* Chrome, Safari, Opera*/
+    padding: 0 2.4rem;
+    margin-bottom: 1.6rem;
+    &__type {
+      &-subtitle {
+        color: var(--muted);
+      }
+    }
+    &__detail {
+      color: var(--text);
+      &__coin {
+        display: flex;
+        align-items: center;
+        &-image {
+          width: 2rem;
+          height: 2rem;
+        }
+        &-amount {
+          padding: 0 0.8rem;
+        }
+      }
+      &-chain {
+        text-align: right;
+      }
     }
   }
 }
