c5e5a7778ef8ec0277ca13afe0f0d31235943fc4
sunghoon <sunghoon@bharvest.io>
2021-06-22 04:01:09 +0900

    WIP: slippage setting modal

---
+++ src/components/ui/SlippageSettingModal.vue
@@ -0,0 +1,153 @@
+<template>
+  <div class="denom-select-modal-wrapper elevation-panel">
+    <GobackWithClose @goback="emitHandler('goback')" @close="emitHandler('close')" />
+
+    <tippy :max-width="192">
+      <HintIcon />
+
+      <template #content> Minimum total received if your entire swap is fulfilled. </template>
+    </tippy>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent, reactive, toRefs } from 'vue';
+
+import GobackWithClose from '@/components/common/headers/GobackWithClose.vue';
+import HintIcon from '@/components/common/Icons/HintIcon.vue';
+
+export default defineComponent({
+  name: 'TxStepsModal',
+  components: {
+    GobackWithClose,
+
+    HintIcon,
+  },
+
+  emits: ['goback', 'close'],
+  setup(props, { emit }) {
+    const processData = reactive({
+      emitHandler: (event) => {
+        emit(event);
+      },
+    });
+
+    return toRefs(processData);
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.denom-select-modal-wrapper {
+  position: relative;
+  width: 100%;
+  /* height: 55.8rem; */
+
+  margin-bottom: 5rem;
+  top: 0;
+  left: 0;
+
+  overflow: hidden;
+
+  background-color: var(--surface);
+  z-index: 10;
+
+  .title {
+    padding: 0 2.4rem 2.4rem;
+    text-align: left;
+  }
+
+  .amount-info {
+    display: flex;
+    justify-content: space-between;
+
+    color: var(--text);
+
+    padding: 0 2.4rem;
+    margin-bottom: 1.6rem;
+    &__type {
+      &-subtitle {
+        color: var(--muted);
+      }
+    }
+    &__detail {
+      color: var(--text);
+      &__coin {
+        display: flex;
+        align-items: center;
+        &-image {
+          width: 2rem;
+          height: 2rem;
+        }
+        &-amount {
+          padding: 0 0.8rem;
+        }
+      }
+      &-chain {
+        text-align: right;
+      }
+    }
+  }
+
+  .divider {
+    margin: 0 2.4rem;
+    height: 1px;
+    background-color: var(--border-trans);
+  }
+
+  .detail {
+    padding: 0 2.4rem;
+    &__title {
+      color: var(--text);
+      padding: 1.6rem 0;
+    }
+
+    &__row {
+      display: flex;
+      justify-content: space-between;
+      padding-bottom: 1.6rem;
+
+      &-key {
+        display: flex;
+        align-items: center;
+        color: var(--muted);
+
+        div {
+          margin-right: 0.4rem;
+        }
+      }
+    }
+  }
+
+  .detail-transfer {
+    @extend .detail;
+
+    .detail__title {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+    }
+
+    .icon {
+      font-size: 1.6rem;
+      color: var(--muted);
+    }
+  }
+
+  .warn {
+    margin: 0 2.4rem;
+    padding: 1.2rem;
+    border: 1px solid var(--border-trans);
+    color: var(--muted);
+    border-radius: 8px;
+  }
+
+  .warn-transfer {
+    border: none;
+    padding: 0 1.2rem;
+  }
+
+  .button-wrapper {
+    padding: 2.8rem 2.4rem 2.4rem;
+  }
+}
+</style>
