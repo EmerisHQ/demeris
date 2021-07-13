c2b573f53ab16e842f3057e331a6d39d7bf8f411
Lucio Rubens <luciorubeens@users.noreply.github.com>
2021-07-13 14:53:00 -0300

    refactor: connect with store

--- src/components/common/SettingsMenu.vue
+++ src/components/common/SettingsMenu.vue
@@ -1,70 +1,34 @@
-<script lang="ts">
-import { defineComponent, reactive, ref } from 'vue';
-
-import Icon from '@/components/ui/Icon.vue';
-import Switch from '@/components/ui/Switch.vue';
-
-export default defineComponent({
-  components: {
-    Icon,
-    Switch,
-  },
-  setup() {
-    const isOpen = ref(false);
-    const toggleOpen = () => (isOpen.value = !isOpen.value);
-
-    const settings = reactive({
-      theme: 'system',
-      customSlippage: false,
-      viewAllAssets: false,
-      lpAssetPool: false,
-    });
-
-    const toggleSetting = (key: string) => {
-      settings[key] = !settings[key];
-    };
-
-    return {
-      isOpen,
-      toggleOpen,
-      toggleSetting,
-      settings,
-    };
-  },
-});
-</script>
-
 <template>
-  <div class="settings-menu" :class="{ 'settings-menu--open': isOpen }">
+  <div ref="menuRef" class="settings-menu" :class="{ 'settings-menu--open': isOpen }">
     <button class="settings-menu__button" @click="toggleOpen">
       <Icon name="MenuIcon" :icon-size="2.2" class="settings-menu__button__icon" />
     </button>
 
     <div v-show="isOpen" class="settings-menu__dropdown elevation-panel">
       <div class="settings-menu__dropdown__item">
-        <span>Theme</span>
+        <span>{{ $t('components.settingsMenu.theme') }}</span>
 
         <select v-model="settings.theme" class="settings-menu__dropdown__button__select">
-          <option value="system">System</option>
-          <option value="light">Light</option>
+          <option value="system">{{ $t('components.settingsMenu.system') }}</option>
+          <option value="light">{{ $t('components.settingsMenu.light') }}</option>
         </select>
       </div>
 
       <hr class="settings-menu__dropdown__divider" />
 
       <div>
-        <p class="settings-menu__dropdown__label">Advanced settings</p>
-        <button class="settings-menu__dropdown__button" @click="toggleSetting('customSlippage')">
-          <span>Custom slippage</span>
-          <Switch v-model="settings.customSlippage" class="settings-menu__dropdown__button__switch" />
+        <p class="settings-menu__dropdown__label">{{ $t('components.settingsMenu.advancedSettings') }}</p>
+        <button class="settings-menu__dropdown__button" @click="toggleSetting('allowCustomSlippage')">
+          <span>{{ $t('components.settingsMenu.customSlippage') }}</span>
+          <Switch v-model="settings.allowCustomSlippage" class="settings-menu__dropdown__button__switch" />
         </button>
-        <button class="settings-menu__dropdown__button" @click="toggleSetting('viewAllAssets')">
-          <span>View all assets</span>
-          <Switch v-model="settings.viewAllAssets" class="settings-menu__dropdown__button__switch" />
+        <button class="settings-menu__dropdown__button" @click="toggleSetting('viewUnverified')">
+          <span>{{ $t('components.settingsMenu.viewAllAssets') }}</span>
+          <Switch v-model="settings.viewUnverified" class="settings-menu__dropdown__button__switch" />
         </button>
-        <button class="settings-menu__dropdown__button" @click="toggleSetting('lpAssetPool')">
-          <span>LP Asset pool</span>
-          <Switch v-model="settings.lpAssetPool" class="settings-menu__dropdown__button__switch" />
+        <button class="settings-menu__dropdown__button" @click="toggleSetting('viewLPAssetPools')">
+          <span>{{ $t('components.settingsMenu.lpAssetPool') }}</span>
+          <Switch v-model="settings.viewLPAssetPools" class="settings-menu__dropdown__button__switch" />
         </button>
       </div>
 
@@ -72,12 +36,12 @@ export default defineComponent({
 
       <div>
         <a href="https://cosmos.network" target="_blank" class="settings-menu__dropdown__button">
-          <span>Support</span>
+          <span>{{ $t('components.settingsMenu.support') }}</span>
           <Icon name="ArrowUpIcon" :icon-size="1.5" class="external-icon" />
         </a>
 
         <a href="https://twitter.com/emerisHQ" target="_blank" class="settings-menu__dropdown__button">
-          <span>Twitter</span>
+          <span>{{ $t('components.settingsMenu.twitter') }}</span>
           <Icon name="ArrowUpIcon" :icon-size="1.5" class="external-icon" />
         </a>
 
@@ -88,13 +52,96 @@ export default defineComponent({
       </div>
 
       <div class="settings-menu__dropdown__list">
-        <router-link to="/" class="settings-menu__dropdown__list__item">Privacy</router-link>
-        <router-link to="/" class="settings-menu__dropdown__list__item">Terms of use</router-link>
-        <router-link to="/" class="settings-menu__dropdown__list__item">Cookies policy</router-link>
+        <router-link to="/" class="settings-menu__dropdown__list__item">
+          {{
+            $t('components.settingsMenu.privacy')
+          }}
+        </router-link>
+        <router-link to="/" class="settings-menu__dropdown__list__item">
+          {{
+            $t('components.settingsMenu.termsOfUse')
+          }}
+        </router-link>
+        <router-link to="/" class="settings-menu__dropdown__list__item">
+          {{
+            $t('components.settingsMenu.cookiesPolicy')
+          }}
+        </router-link>
       </div>
     </div>
   </div>
 </template>
+
+<script lang="ts">
+import { computed, defineComponent, onMounted, onUnmounted, reactive, ref } from 'vue';
+import { useStore } from 'vuex';
+
+import Icon from '@/components/ui/Icon.vue';
+import Switch from '@/components/ui/Switch.vue';
+import { GlobalDemerisActionTypes } from '@/store/demeris/action-types';
+
+export default defineComponent({
+  components: {
+    Icon,
+    Switch,
+  },
+  setup() {
+    const store = useStore();
+    const menuRef = ref(null);
+
+    const isOpen = ref(false);
+    const toggleOpen = () => (isOpen.value = !isOpen.value);
+
+    const updateSession = (key: string, value: any) => {
+      store.dispatch(GlobalDemerisActionTypes.SET_SESSION_DATA, { data: { [key]: value } });
+    };
+
+    const settings = reactive({
+      theme: 'system',
+      allowCustomSlippage: computed({
+        get: () => store.getters['demeris/allowCustomSlippage'],
+        set: (value: boolean) => updateSession('customSlippage', value),
+      }),
+      viewUnverified: computed({
+        get: () => store.getters['demeris/viewUnverified'],
+        set: (value: boolean) => updateSession('viewUnverified', value),
+      }),
+      viewLPAssetPools: computed({
+        get: () => store.getters['demeris/viewLPAssetPools'],
+        set: (value: boolean) => updateSession('viewLPAssetPools', value),
+      }),
+    });
+
+    const toggleSetting = (key: string) => {
+      settings[key] = !settings[key];
+    };
+
+    const clickOutsideListener = (event: Event) => {
+      if (event.composedPath().includes(menuRef.value)) {
+        return;
+      }
+
+      isOpen.value = false;
+    };
+
+    onMounted(() => {
+      window.addEventListener('pointerdown', clickOutsideListener);
+    });
+
+    onUnmounted(() => {
+      window.removeEventListener('pointerdown', clickOutsideListener);
+    });
+
+    return {
+      isOpen,
+      menuRef,
+      settings,
+      toggleOpen,
+      toggleSetting,
+    };
+  },
+});
+</script>
 
 <style lang="scss">
 .settings-menu {
