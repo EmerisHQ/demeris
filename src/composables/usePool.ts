dee6689eb38f1cefedcd40a53fcb0ab94425e290
Peng Zhong <172531+nylira@users.noreply.github.com>
2021-07-20 15:43:11 -0400

    Revert "refactor: add total pooled amount (#210)" (#215)
    This reverts commit 0dd5c921f0cbfe68728b2f2dc667c3f526486d7d.

--- src/composables/usePool.ts
+++ src/composables/usePool.ts
@@ -4,15 +4,11 @@ import { useAllStores } from '@/store';
 
 import usePools from './usePools';
 
-type Options = {
-  autoUpdate?: boolean;
-};
-
-export default function usePool(id?: string | ComputedRef<string>, options: Options = { autoUpdate: true }) {
+export default function usePool(id?: string | ComputedRef<string>) {
   const store = useAllStores();
   const reserveBaseDenoms = ref([]);
 
-  const { poolById, formatPoolName, poolPriceById, getReserveBaseDenoms, getPoolMeta } = usePools();
+  const { poolById, formatPoolName, poolPriceById, getReserveBaseDenoms } = usePools();
 
   const pool = computed(() => {
     const poolId = unref(id);
@@ -87,17 +83,14 @@ export default function usePool(id?: string | ComputedRef<string>, options: Opti
      * WithdrawAmount = ReserveAmount * PoolCoinAmount * WithdrawFeeProportion / TotalSupply
      * @see https://github.com/tendermint/liquidity/blob/develop/x/liquidity/keeper/liquidity_pool.go#L407
      */
-    const reserveDenoms = getPoolMeta(pool.value.id)?.reserveBaseDenoms || [];
 
     const withdrawCoins = [
       {
         amount: (reserveBalances.value[0].amount * poolCoinAmount) / totalSupply.value,
-        base_denom: reserveDenoms[0],
         denom: reserveBalances.value[0].denom,
       },
       {
         amount: (reserveBalances.value[1].amount * poolCoinAmount) / totalSupply.value,
-        base_denom: reserveDenoms[1],
         denom: reserveBalances.value[1].denom,
       },
     ];
@@ -108,9 +101,7 @@ export default function usePool(id?: string | ComputedRef<string>, options: Opti
   watch(
     pool,
     () => {
-      if (options.autoUpdate) {
-        updateReserveBalances();
-      }
+      updateReserveBalances();
       setPairName();
     },
     { immediate: true },
