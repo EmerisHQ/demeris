d3d0f001870beec6b24c4e58c34287c962778f89
gamarin2 <gautier@tendermint.com>
2021-07-27 16:20:50 +0200

    fix develop

--- src/App.vue
+++ src/App.vue
@@ -29,7 +29,7 @@ export default defineComponent({
     document.documentElement.setAttribute('color-theme', 'light');
 
     await this.$store.dispatch(GlobalDemerisActionTypes.INIT, {
-      endpoint: 'https://staging.demeris.io/v1',
+      endpoint: 'https://dev.demeris.io/v1',
       hub_chain: 'cosmos-hub',
       refreshTime: 5000,
       gas_limit: 300000,
@@ -58,7 +58,7 @@ export default defineComponent({
       });
     }
     await this.$store.dispatch('common/env/config', {
-      apiNode: 'https://staging.demeris.io/v1/liquidity',
+      apiNode: 'https://dev.demeris.io/v1/liquidity',
       rpcNode: null,
       wsNode: null,
       chainId: 'cosmos-hub',
