39db8bab5ebd900d60b8ff4319d64750891234ef
Lucio Rubens <luciorubeens@users.noreply.github.com>
2021-08-05 18:47:09 -0300

    refactor: format precision

--- src/components/common/FeeLevelSelector.vue
+++ src/components/common/FeeLevelSelector.vue
@@ -229,7 +229,6 @@ export default defineComponent({
       fees[GasPriceLevel.LOW] = lowFeeUSD.value;
       fees[GasPriceLevel.AVERAGE] = avgFeeUSD.value;
       fees[GasPriceLevel.HIGH] = highFeeUSD.value;
-
       return fees;
     });
 
@@ -298,14 +297,6 @@ export default defineComponent({
         data.isFeesOpen = !data.isFeesOpen;
       },
       GasPriceLevel,
-      formatter: new Intl.NumberFormat('en-US', {
-        style: 'currency',
-        currency: 'USD',
-
-        // These options are needed to round to whole numbers if that's what you want.
-        //minimumFractionDigits: 0, // (this suffices for whole numbers, but will print 2500.10 as $2,500.1)
-        //maximumFractionDigits: 0, // (causes 2500.99 to be printed as $2,501)
-      }),
       feeIconColor: getComputedStyle(document.body).getPropertyValue('--inactive'),
     });
 
@@ -318,7 +309,36 @@ export default defineComponent({
       emit('update:fees', feeMap[props.gasPriceLevel]);
     });
 
-    return { ...toRefs(data), txCount, fees, swapDollarFee, isPoolCoin, poolCoinSwapFee, poolCoinDisplayDenom };
+    const formatter = computed(() => {
+      let precisions = [];
+      for (const chain_name in lowFee.value) {
+        for (const denom in lowFee.value[chain_name]) {
+          precisions.push(store.getters['demeris/getDenomPrecision']({ name: denom }));
+        }
+      }
+
+      const maxPrecision = Math.max(...precisions.filter(Boolean));
+
+      return new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+        maximumFractionDigits: maxPrecision,
+        // These options are needed to round to whole numbers if that's what you want.
+        //minimumFractionDigits: 0, // (this suffices for whole numbers, but will print 2500.10 as $2,500.1)
+        //maximumFractionDigits: 0, // (causes 2500.99 to be printed as $2,501)
+      });
+    });
+
+    return {
+      ...toRefs(data),
+      formatter,
+      txCount,
+      fees,
+      swapDollarFee,
+      isPoolCoin,
+      poolCoinSwapFee,
+      poolCoinDisplayDenom,
+    };
   },
 });
 </script>
