d1690d2ed996d1c2649200a7756f93dd50508350
Peng Zhong <172531+nylira@users.noreply.github.com>
2021-08-06 12:02:37 -0600

    feat/pool ratio  (#367)
    * add pool pair name to pool asset page
    
    * start fixing ratio ticker
    
    * working token ratio

--- src/components/assets/AssetsTable/PoolBanner.vue
+++ src/components/assets/AssetsTable/PoolBanner.vue
@@ -2,8 +2,8 @@
   <Alert v-if="pool" status="info" class="mt-4" size="md" :show-icon="false">
     <h5 class="font-bold text-text">What is <Denom :name="name" />?</h5>
     <p class="mt-3 text-muted -text-1 leading-copy">
-      <Denom :name="name" /> (<Ticker :name="name" />) is a liquidity pool (LP) asset. LP assets represent shares of a
-      particular liquidity pool.
+      <Denom :name="name" /> (<Ticker :name="name" />) is a liquidity pool (LP) asset. This token represents a share of
+      the <strong>{{ pairName }}</strong> liquidity pool.
     </p>
     <p class="mt-3">
       <a
@@ -22,6 +22,7 @@ import { useRouter } from 'vue-router';
 import Denom from '@/components/common/Denom.vue';
 import Ticker from '@/components/common/Ticker.vue';
 import Alert from '@/components/ui/Alert.vue';
+import usePool from '@/composables/usePool';
 import { useAllStores } from '@/store';
 
 export default defineComponent({
@@ -50,11 +51,14 @@ export default defineComponent({
       return pools.value.find((pool) => pool.pool_coin_denom == props.name);
     });
 
+    const { pairName } = usePool(computed(() => pool.value.id as string));
+
     const openPoolPage = () => {
       router.push({ name: 'Pool', params: { id: pool.value.id } });
     };
 
     return {
+      pairName,
       pool,
       openPoolPage,
     };
