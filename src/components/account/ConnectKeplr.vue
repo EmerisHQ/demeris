30d3e3720a610804429b7941bc1bc7cd3df4de45
Peng Zhong <172531+nylira@users.noreply.github.com>
2021-08-06 10:46:35 -0600

    feat/landing flow (#329)
    * refactor css and add AgreeWarning
    
    * fix not compatible issue
    
    * working local storage
    
    * start on graphic
    
    * working portal graphic
    
    * improve logic and design fixes
    
    * improve design
    
    * improve design
    
    * fixes and improvements
    
    * mobile popup
    
    * delete old welcome page
    
    * final text for ToS
    
    * update design for tos
    
    * link to real privacy, terms, cookies urls
    
    * make demo skip past welcome
    
    * remove extra
    
    * auto connect after agree
    
    * fix logic bugs
    
    * minor padding issue
    
    * use color variables
    
    * fix bug with terms/privacy/cookies link?

--- src/components/account/ConnectKeplr.vue
+++ src/components/account/ConnectKeplr.vue
@@ -1,10 +1,18 @@
 <template>
   <div class="connect-keplr">
-    <div class="flex flex-col pb-8 px-8 text-center">
+    <div class="flex flex-col px-8 text-center" :class="type == 'welcome' ? 'py-8' : 'pb-8'">
       <template v-if="!isConnecting">
         <slot name="title">
-          <img src="~@/assets/images/keplr-wallet-logo.png" alt="Keplr logo" class="w-12 mx-auto mb-8" />
-          <h2 class="text-3 font-bold">{{ $t('wallet.connect.modal1.title') }}</h2>
+          <img
+            v-if="type !== 'welcome'"
+            src="~@/assets/images/keplr-wallet-logo.png"
+            alt="Keplr logo"
+            class="w-12 mx-auto mb-8"
+          />
+          <h2 v-if="type === 'welcome'" class="text-3 font-bold">
+            {{ $t('generic_cta.connectToEmeris') }}
+          </h2>
+          <h2 v-else class="text-3 font-bold">{{ $t('wallet.connect.modal1.title') }}</h2>
         </slot>
 
         <div class="flex-1 mt-8 leading-copy text-muted space-y-4">
@@ -13,16 +21,21 @@
           </slot>
         </div>
 
-        <div class="flex flex-col mt-12">
-          <Button :name="$t('wallet.connect.modal1.button')" @click="signIn" />
-
+        <div class="flex items-center flex-col mt-12">
+          <Button :name="$t('wallet.connect.modal1.button')" @click="trySignIn" />
           <a
-            href="https://t.me/EmerisHQ"
-            target="_blank"
-            rel="noreferrer noopener"
-            class="mt-4 text-muted hover:text-text -text-1 p-1.5 transition-colors active:opacity-70"
+            v-if="type === 'welcome'"
+            class="mt-4 font-medium hover:text-text p-1.5 transition-colors active:opacity-70"
+            @click="emitTryDemo"
           >
-            {{ $t('wallet.connect.modal1.needHelp') }}
+            {{ $t('generic_cta.tryTheDemo') }}
+          </a>
+          <a
+            v-else
+            class="mt-4 font-medium hover:text-text p-1.5 transition-colors active:opacity-70"
+            @click="emitCancel"
+          >
+            {{ $t('generic_cta.cancel') }}
           </a>
         </div>
       </template>
@@ -32,6 +45,7 @@
           <Spinner :size="3" />
           <span class="mt-6 text-muted">{{ $t('wallet.connect.modal1.opening') }}</span>
           <p class="text-3 font-bold mt-2">{{ $t('wallet.connect.modal1.connecting') }}</p>
+          <span class="mt-6 text-muted">{{ $t('wallet.connect.modal1.connectingHelp') }}</span>
         </div>
         <Button variant="link" :name="$t('generic_cta.cancel')" :click-function="cancel" class="mt-12" />
       </div>
@@ -40,7 +54,7 @@
 </template>
 
 <script lang="ts">
-import { computed, defineComponent, ref, watch } from 'vue';
+import { computed, defineComponent, onMounted, ref, watch } from 'vue';
 import { useStore } from 'vuex';
 
 import Button from '@/components/ui/Button.vue';
@@ -56,15 +70,28 @@ export default defineComponent({
     Spinner,
   },
 
-  emits: ['cancel', 'connect'],
+  props: {
+    type: {
+      type: String,
+      default: undefined,
+    },
+  },
+
+  emits: ['cancel', 'connect', 'warning', 'try-demo'],
 
   setup(_, { emit }) {
     const store = useStore();
     const isConnecting = ref(false);
+    const isWarningAgreed = ref(null);
+    const isWarningNeeded = ref(null);
 
     const emitCancel = () => {
       cancel();
       emit('cancel');
+    };
+
+    const emitTryDemo = () => {
+      emit('try-demo');
     };
 
     const cancel = () => {
@@ -75,10 +102,23 @@ export default defineComponent({
       return store.getters['demeris/isSignedIn'];
     });
 
+    const trySignIn = () => {
+      if (isWarningAgreed.value) {
+        signIn();
+      } else {
+        emit('warning');
+      }
+    };
+
     const signIn = () => {
       store.dispatch(GlobalDemerisActionTypes.SIGN_IN);
       isConnecting.value = true;
     };
+
+    onMounted(() => {
+      isWarningAgreed.value = window.localStorage.getItem('isWarningAgreed');
+      isWarningNeeded.value = window.localStorage.getItem('isWarningNeeded');
+    });
 
     watch(isSignedIn, () => {
       if (isSignedIn.value) {
@@ -86,9 +126,12 @@ export default defineComponent({
       }
     });
 
-    return { isConnecting, emitCancel, cancel, signIn };
+    return { isConnecting, emitCancel, cancel, signIn, trySignIn, emitTryDemo };
   },
 });
 </script>
-
-<style lang="scss"></style>
+<style lang="scss" scoped>
+a {
+  cursor: pointer;
+}
+</style>
