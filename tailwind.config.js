96b54dda6f4e4a6f3ca5d673c9d87b6c5c2142d2
Peng Zhong <172531+nylira@users.noreply.github.com>
2021-08-16 13:25:55 -0400

    disable production purge of css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: { content: ['./public/**/*.html', './src/**/*.vue'] },
+  purge: { content: ['./public/**/*.html'] },
   // mode: 'jit',
   darkMode: 'class', // or 'media' or 'class'
   theme: {
