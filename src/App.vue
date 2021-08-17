e4afe883928f7a73f0ff3e630f9e25f5a994311f
Peng Zhong <172531+nylira@users.noreply.github.com>
2021-08-17 09:40:15 -0400

    fix endpoint

--- src/App.vue
+++ src/App.vue
@@ -29,7 +29,7 @@ export default defineComponent({
         window.localStorage.setItem('gasLimit', gasLimit.toString());
       }
       await store.dispatch(GlobalDemerisActionTypes.INIT, {
-        endpoint: 'https://dev.demeris.io/v1',
+        endpoint: 'https://staging.demeris.io/v1',
         hub_chain: 'cosmos-hub',
         refreshTime: 5000,
         gas_limit: gasLimit,
