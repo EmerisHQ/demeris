58a00194be83564a09f01eb2d12e27ee8f6b0c03
Peng Zhong <172531+nylira@users.noreply.github.com>
2021-08-19 12:40:38 -0400

    add google analytics tracking

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import 'tippy.js/dist/tippy.css';
 import vueLib from '@starport/vue';
 import mitt from 'mitt';
 import { createApp } from 'vue';
+import VueGtag from 'vue-gtag';
 import { createI18n } from 'vue-i18n';
 import { createMetaManager } from 'vue-meta';
 import VueTippy from 'vue-tippy';
@@ -27,7 +28,15 @@ const emitter = mitt();
 const app = createApp(App);
 app.config.globalProperties.emitter = emitter;
 app.config.globalProperties._depsLoaded = true;
-app.use(i18n).use(store).use(router).use(vueLib).use(VueTippy).use(createMetaManager()).mount('#app');
+app
+  .use(i18n)
+  .use(store)
+  .use(router)
+  .use(vueLib)
+  .use(VueTippy)
+  .use(createMetaManager())
+  .use(VueGtag, { config: { id: 'UA-201374903-1' } })
+  .mount('#app');
 
 app.config.globalProperties.$filters = {
   ...stringFilters,
