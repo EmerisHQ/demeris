8b80613094cf8592b25f41741a3c3b5ba0d20c83
Clockwork <alex@clockwork.gr>
2021-08-19 13:35:18 +0300

    refactor: Simplify denom handling

---
+++ src/composables/useDenoms.ts
@@ -0,0 +1,28 @@
+import { computed, ref, watch } from 'vue';
+
+import { useAllStores } from '@/store';
+import { getDisplayName } from '@/utils/actionHandler';
+import { getTicker } from '@/utils/actionHandler';
+
+export default function useDenoms() {
+  const store = useAllStores();
+
+  const verifiedDenoms = computed(() => {
+    return store.getters['demeris/getVerifiedDenoms'];
+  });
+  const useDenom = (base_denom) => {
+    const price = computed(() => {
+      return store.getters['demeris/getPrice']({ denom: base_denom });
+    });
+    const displayName = ref('-');
+    const tickerName = ref('-');
+    const updateDenom = async () => {
+      tickerName.value = await getTicker(base_denom, store.getters['demeris/getDexChain']);
+      displayName.value = await getDisplayName(base_denom, store.getters['demeris/getDexChain']);
+    };
+
+    watch(verifiedDenoms, updateDenom);
+    return { price, displayName, tickerName };
+  };
+  return { useDenom, verifiedDenoms };
+}
