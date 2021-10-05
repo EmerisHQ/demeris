06269abdc043a130bd4cf061e81226261f26d14f
Lucio Rubens <luciorubeens@users.noreply.github.com>
2021-10-05 08:46:54 -0300

    fix: prevent zero decimals

--- src/components/ui/CurrencyDisplay.vue
+++ src/components/ui/CurrencyDisplay.vue
@@ -3,7 +3,7 @@
     <span>-</span>
   </template>
 
-  <i18n-n v-else tag="span" :value="value" format="currency">
+  <i18n-n v-else tag="span" :value="inputValue" format="currency">
     <template #currency="slotProps">
       <span>{{ slotProps.currency }}</span>
     </template>
@@ -24,6 +24,7 @@
 </template>
 
 <script lang="ts">
+import BigNumber from 'bignumber.js';
 import { computed, defineComponent } from 'vue';
 
 export default defineComponent({
@@ -40,10 +41,36 @@ export default defineComponent({
       type: Boolean,
       default: false,
     },
+    preventZero: {
+      type: Boolean,
+      default: false,
+    },
   },
   setup(props) {
     const hasValue = computed(() => !!props.value);
-    return { hasValue };
+
+    const inputValue = computed(() => {
+      if (!props.preventZero) {
+        return props.value;
+      }
+
+      const bgValue = new BigNumber(props.value);
+
+      let decimalPlaces = 2;
+      const maxDecimalPlaces = 8;
+
+      // This will prevent formatting smaller values like 0.0001 to 0.00
+      while (bgValue.decimalPlaces(decimalPlaces).isZero()) {
+        decimalPlaces++;
+        if (decimalPlaces === maxDecimalPlaces) {
+          break;
+        }
+      }
+
+      return bgValue.decimalPlaces(decimalPlaces).toNumber();
+    });
+
+    return { hasValue, inputValue };
   },
 });
 </script>
