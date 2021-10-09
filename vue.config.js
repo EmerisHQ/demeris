f44154fbe1320d2dddf51836c63eee5f0aa5186c
Fabian Weber <fabian.weber@tendermint.com>
2021-10-09 22:34:42 +0300

    v1

--- vue.config.js
+++ vue.config.js
@@ -6,4 +6,12 @@ process.env.VUE_APP_VERSION = pkg.version;
 
 module.exports = {
   transpileDependencies: ['@starport/tendermint-liquidity-js', 'vue-meta'],
+  publicPath: process.env.ELECTRON === 'true' ? './' : '/',
+  pluginOptions: {
+    electronBuilder: {
+      builderOptions: {
+        // options placed here will be merged with default configuration and passed to electron-builder
+      },
+    },
+  },
 };
