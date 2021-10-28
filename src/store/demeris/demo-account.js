361c516c7c03208f6fb3a7a757d7686ca7767e71
Alex Megalokonomos <alex@clockwork.gr>
2021-10-29 00:23:17 +0300

    hotfix: add juno demo account

--- src/store/demeris/demo-account.js
+++ src/store/demeris/demo-account.js
@@ -84,4 +84,5 @@ export const demoAddresses = {
   emoney: 'emoney10ms58lgajy69z2x62shj0nxc6r3a0r7qxkzhm2',
   terra: 'terra1gwk96n7w48e35shz3wdewmv5gpzfwdn28y9pkn',
   likecoin: 'cosmos10ms58lgajy69z2x62shj0nxc6r3a0r7qf4crvh',
+  juno: 'juno10ms58lgajy69z2x62shj0nxc6r3a0r7ql8mctt',
 };
