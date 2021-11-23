e2e9e61cdf61a026db9b8f04711705df0903ce29
Alex Megalokonomos <alex.megalokonomos@tendermint.com>
2021-11-23 13:15:24 +0200

    fix: Correct Withdraw Liquidity summary display (#727)

--- src/components/wizard/previews/PreviewWithdrawLiquidity.vue
+++ src/components/wizard/previews/PreviewWithdrawLiquidity.vue
@@ -110,8 +110,8 @@ export default defineComponent({
         const pool = getPoolById(props.response.pool_id);
         const poolCoin = { amount: props.response.pool_coin_amount, denom: props.response.pool_coin_denom };
         const precisions = {
-          coinA: store.getters['demeris/getDenomPrecision']({ name: pool.denoms[0] }) ?? 6,
-          coinB: store.getters['demeris/getDenomPrecision']({ name: pool.denoms[1] }) ?? 6,
+          coinA: store.getters['demeris/getDenomPrecision']({ name: pool.reserveBaseDenoms[0] }) ?? 6,
+          coinB: store.getters['demeris/getDenomPrecision']({ name: pool.reserveBaseDenoms[1] }) ?? 6,
         };
 
         return { pool, poolCoin, precisions };
