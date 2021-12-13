c4e466c9243ae195909cc3074f73106a65a6da7c
michalBinarApps <84776925+michalBinarApps@users.noreply.github.com>
2021-12-13 11:02:56 +0100

    Update welcome-page.js

--- cypress/support/pages/welcome-page.js
+++ cypress/support/pages/welcome-page.js
@@ -6,19 +6,11 @@ export class WelcomePage {
   tryTheDemoButtonIsVisible() {
     return this.tryTheDemoButton().should('be.visible');
   }
-
   tryTheDemoButton() {
-    //cy.get('[data-cy="tryTheDemoButton"]', { timeout: 9900000 }).as('guzik')
-    //cy.wait('@guzik')
-    // cy.get('[data-cy="tryTheDemoButton"]', { timeout: 9900000 }).eq(0)
-    return cy.get('[data-cy="tryTheDemoButton"]'); //, { timeout: 9900000 }
+    return cy.get('[data-cy="tryTheDemoButton"]', { timeout: 20000 }); 
   }
 
   centerContentWraper() {
-    // cy.waitUntil()
-    //*[contains(@class, 'flex flex-col px-8 text-center py-8')]
-    // '*[class^="flex flex-col px-8 text-center py-8"]'
-    cy.waitFor('*[class^="flex flex-col px-8 text-center py-8"]');
-    return cy.get('*[class^="flex flex-col px-8 text-center py-8"]');
+    return cy.get('*[class^="flex flex-col px-8 text-center py-8"]', { timeout: 20000 });
   }
 }
