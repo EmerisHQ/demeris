4344b4517ed39952d662b1466409efd5816ca7af
Balogun Abdulrahman <balogunabdulrahman@Baloguns-MacBook-Pro.local>
2022-01-17 15:19:04 +0100

    Worked on Integration of token price performance chart

--- src/components/common/charts/AreaChart.vue
+++ src/components/common/charts/AreaChart.vue
@@ -6,6 +6,7 @@
         v-for="(item, index) in filterItems"
         :key="index"
         class="mx-2 rounded px-4 py-2 -text-1 cursor-pointer hover:bg-fg"
+        :class="item.value === activeFilterItem ? 'bg-fg' : ''"
         @click="setActiveFilter(item)"
       >
         {{ item.text }}
@@ -15,7 +16,9 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType } from 'vue';
+import { defineComponent, PropType, ref, watch } from 'vue';
+
+import { TokenPrices } from '@/types/api';
 
 export default defineComponent({
   name: 'AreaChart',
@@ -36,90 +39,111 @@ export default defineComponent({
       default: () => [],
     },
   },
-  setup() {
-    return {
-      filterItems: [
-        {
-          text: '1D',
-          value: 'day',
+  emits: ['filterChanged'],
+  setup(props, { emit }) {
+    const filterItems = ref([
+      {
+        text: '1D',
+        value: '1',
+      },
+      {
+        text: '1W',
+        value: '7',
+      },
+      {
+        text: '2W',
+        value: '14',
+      },
+      {
+        text: '1M',
+        value: '30',
+      },
+      {
+        text: '3M',
+        value: '90',
+      },
+      {
+        text: '1Y',
+        value: '365',
+      },
+      {
+        text: 'All',
+        value: 'max',
+      },
+    ]);
+
+    const chartData = ref({
+      options: {
+        chart: {
+          type: 'area',
+          toolbar: {
+            show: false,
+          },
         },
-        {
-          text: '1W',
-          value: 'week',
+        stroke: {
+          curve: 'straight',
+          width: 2,
         },
-        {
-          text: '1M',
-          value: 'month',
+        xaxis: {
+          labels: {
+            show: false,
+          },
+          axisBorder: {
+            show: false,
+          },
+          axisTicks: {
+            show: false,
+          },
         },
-        {
-          text: '1Y',
-          value: 'year',
+        dataLabels: {
+          enabled: false,
         },
+        colors: ['#00CF30'],
+        fill: {
+          colors: ['#90EE90'],
+          gradient: {
+            type: 'vertical',
+            shade: 'light',
+            opacityFrom: 0.7,
+            opacityTo: 0.3,
+          },
+        },
+        yaxis: {
+          show: false,
+        },
+        grid: {
+          show: false,
+        },
+      },
+      series: [
         {
-          text: 'All',
-          value: 'all',
+          name: 'Price',
+          data: [],
         },
       ],
-      chartData: {
-        options: {
-          chart: {
-            type: 'area',
-            toolbar: {
-              show: false,
-            },
-          },
-          stroke: {
-            curve: 'straight',
-            width: 2,
-          },
-          xaxis: {
-            labels: {
-              show: false,
-            },
-            axisBorder: {
-              show: false,
-            },
-            axisTicks: {
-              show: false,
-            },
-          },
-          dataLabels: {
-            enabled: false,
-          },
-          colors: ['#00CF30'],
-          fill: {
-            colors: ['#90EE90'],
-            gradient: {
-              type: 'vertical',
-              shade: 'light',
-              opacityFrom: 0.7,
-              opacityTo: 0.3,
-            },
-          },
-          yaxis: {
-            show: false,
-          },
-          grid: {
-            show: false,
-          },
-        },
-        series: [
-          {
-            name: 'Price',
-            data: [],
-          },
-        ],
-      },
-      activeFilterItemValue: null,
+    });
+
+    const activeFilterItem = ref('1');
+
+    const setActiveFilter = (filterObject): void => {
+      activeFilterItem.value = filterObject.value;
+      emit('filterChanged', activeFilterItem.value);
     };
-  },
-  mounted() {
-    this.chartData.series[0].data = this.dataStream;
-  },
-  methods: {
-    setActiveFilter(filterObject): void {
-      this.activeFilterItemValue = filterObject.value;
-    },
+
+    watch(
+      () => props.dataStream as TokenPrices[],
+      async (value) => {
+        chartData.value.series[0].data = value;
+      },
+      { immediate: true },
+    );
+
+    return {
+      filterItems,
+      chartData,
+      activeFilterItem,
+      setActiveFilter,
+    };
   },
 });
 </script>
