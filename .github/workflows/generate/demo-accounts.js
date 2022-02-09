7b8d35d0c87eb419ae2c108cfce1017fd64a3c0e
cauduro.dev <60057887+caudurodev@users.noreply.github.com>
2022-02-09 11:32:01 +0100

    Retrieve demo wallet chain addresses

--- .github/workflows/generate/demo-accounts.js
+++ .github/workflows/generate/demo-accounts.js
@@ -1,21 +1,45 @@
+// TODO: discuss where this script can be placed
+
 // List of chains: https://staging.demeris.io/v1/chains
 // Chain data using chain_name from above endpoint: https://staging.demeris.io/v1/chain/cosmos-hub
 
 const fs = require('fs');
 const axios = require('axios');
+const { Secp256k1HdWallet } = require('@cosmjs/amino');
+const { stringToPath } = require('@cosmjs/crypto');
 
 const file = `${__dirname}/../../../src/store/demeris-user/demo-account.js`;
+
+// TODO: read from env
+const mnemonic = 'nominee fox avoid drive fringe capital main shaft sample basic flag view';
 
 const getDemoAccount = async () => {
   const {
     data: { chains },
   } = await axios.get('https://staging.demeris.io/v1/chains');
-  const chainNames = chains.map((c) => c.chain_name);
-  console.log(chainNames);
+  const chainNames = chains?.map((c) => c.chain_name);
 
-  const { data: chain } = await axios.get(`https://staging.demeris.io/v1/chain/${chainNames[0]}`);
+  const demoAccountAddresses = {};
+  await Promise.all(
+    chainNames.map(async (chainName) => {
+      const {
+        data: { chain },
+      } = await axios.get(`https://staging.demeris.io/v1/chain/${chainName}`);
+      const addressPrefix = chain.node_info.bech32_config.main_prefix;
+      const derivationPath = chain.derivation_path;
 
-  console.log('single chain', chain);
+      const signer = await Secp256k1HdWallet.fromMnemonic(mnemonic, {
+        hdPaths: [stringToPath(derivationPath)],
+        prefix: addressPrefix,
+      });
+      const accountFromSigner = (await signer.getAccounts())[0];
+      const address = accountFromSigner.address;
+
+      demoAccountAddresses[chainName] = address;
+    }),
+  );
+
+  console.log('demoAccountAddresses', demoAccountAddresses);
 
   return [];
 };
