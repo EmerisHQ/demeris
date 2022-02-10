d118ac12e03f832b20f953a9043b57b613615083
MohBorLarge <busram2009@gmail.com>
2022-02-10 15:17:18 +0100

    started code to retrieve json data for Airdrop projects from github repo

--- src/components/layout/Navbar.vue
+++ src/components/layout/Navbar.vue
@@ -23,24 +23,33 @@
     </router-link>
     <router-link
       v-if="isAirdropsFeatureRunning"
-      class="h-12 py-3.5 px-2 md:px-3 leading-5 hover:text-text active:opacity-70"
+      class="h-12 py-3.5 px-2 md:px-3 leading-5 hover:text-text active:opacity-70 airdrop-menu-item"
       exact-active-class="text-text font-medium"
       to="/airdrops"
     >
       {{ $t('navbar.airdrops') }}
-      <svg height="100" width="100" class="blinking">
-        <circle cx="70" cy="20" r="5" fill="red" />
-      </svg>
+      <span class="blinking"></span>
+
+      <div class="airdrop-hover py-3 px-4 mt-3 rounded-lg">
+        <div class="flex items-center">
+          <StarsIcon />
+          <p class="-text-1 ml-1">3 airdrops found</p>
+        </div>
+      </div>
     </router-link>
   </nav>
 </template>
 <script lang="ts">
 import { defineComponent } from 'vue';
 
+import StarsIcon from '@/components/common/Icons/StarsIcon.vue';
 import { featureRunning } from '@/utils/FeatureManager';
 
 export default defineComponent({
   name: 'Navbar',
+  components: {
+    StarsIcon,
+  },
 
   setup() {
     const isAirdropsFeatureRunning = featureRunning('AIRDROPS_FEATURE') ? true : false;
@@ -57,9 +66,29 @@ export default defineComponent({
   color: var(--text);
 }
 
+.airdrop-menu-item {
+  .airdrop-hover {
+    display: none;
+  }
+  &:hover {
+    .airdrop-hover {
+      position: relative;
+      left: -25%;
+      background: black;
+      color: white;
+      display: block;
+    }
+  }
+}
+
 .blinking {
+  height: 10px;
+  width: 10px;
+  background-color: red;
+  border-radius: 50%;
+  display: inline-block;
   position: relative;
-  top: -40px;
+  top: -10px;
   -webkit-animation: 1.3s blink ease infinite;
   -moz-animation: 1.3s blink ease infinite;
   -ms-animation: 1.3s blink ease infinite;
