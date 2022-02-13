ed18e33953cfa46e5a6a56527491a956282c35a1
Pranay Baldev <pranaybaldev@gmail.com>
2022-02-13 23:19:17 +0530

    start

---
+++ src/components/common/SwapRoute.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>Swap Route</div>
+  <div>X transc across Y chains</div>
+  <Timeline />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+import Timeline from '@/components/ui/Timeline.vue';
+
+export default defineComponent({
+  name: 'SwapRoute',
+  components: {
+    Timeline,
+  },
+  props: {},
+  setup() {
+    console.log('some');
+  },
+});
+</script>
+<style lang="scss" scoped></style>
---
+++ src/components/ui/Timeline.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="flex flex-col">timeline</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'SwapRoute',
+  components: {},
+  props: {},
+  setup() {
+    console.log('some');
+  },
+});
+</script>
+<style lang="scss" scoped></style>
