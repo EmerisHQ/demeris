00681e09e3b3bd723b1adc89f32d162202072bdb
Pranay Baldev <pranaybaldev@gmail.com>
2022-02-14 18:01:25 +0530

    saving progress

--- src/components/common/SwapRoute.vue
+++ src/components/common/SwapRoute.vue
@@ -1,13 +1,28 @@
 <template>
   <div>Swap Route</div>
   <div>X transc across Y chains</div>
-  <Timeline />
+  <Timeline
+    :items="[
+      { denom: 'atom', chain: 'somcahin', subItems: ['Transfer x', 'Swap Y'] },
+      { denom: 'iris', chain: 'irischain', subItems: ['Transfer x', 'Swap Y'] },
+      { denom: 'osmo', chain: 'osmo', subItems: ['Transfer x', 'Swap Y'] },
+      { denom: 'lastcoin', chain: 'lastchain' },
+    ]"
+  >
+    <template #item="{ denom, chain, subItems }">
+      <span>icon {{ denom }} &bull; {{ chain }}</span>
+      <div v-for="subItem in subItems" :key="subItem">
+        <span>sub fixed icon</span>
+        <span>{{ subItem }}</span>
+      </div>
+    </template>
+  </Timeline>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 
-import Timeline from '@/components/ui/Timeline.vue';
+import Timeline from '@/components/common/Timeline.vue';
 
 export default defineComponent({
   name: 'SwapRoute',
---
+++ src/components/common/Timeline.vue
@@ -0,0 +1,29 @@
+<template>
+  <div v-for="item in items" :key="item" class="flex flex-col timeline-block p-4">
+    <slot name="item" v-bind="item"></slot>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'SwapRoute',
+  components: {},
+  props: {
+    items: {
+      type: Array,
+      default: null,
+    },
+  },
+  setup() {
+    console.log('some');
+  },
+});
+</script>
+<style lang="scss" scoped>
+.timeline-block {
+  border-left: 4px;
+  border-left-width: thick;
+}
+</style>
