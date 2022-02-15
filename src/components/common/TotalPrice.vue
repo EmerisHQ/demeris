fca32ef646fdd8fd3b284374e14df3eb91b0a63a
Clockwork <alex@clockwork.gr>
2022-02-15 11:35:58 +0200

    feat: refactor total price value calculations

--- src/components/common/TotalPrice.vue
+++ src/components/common/TotalPrice.vue
@@ -4,7 +4,7 @@
   </div>
 </template>
 <script lang="ts">
-import { computed, defineComponent, PropType } from 'vue';
+import { computed, defineComponent, PropType, toRefs } from 'vue';
 import { useStore } from 'vuex';
 
 import CurrencyDisplay from '@/components/ui/CurrencyDisplay.vue';
@@ -26,11 +26,13 @@ export default defineComponent({
   },
   setup(props) {
     const store = useStore();
-
+    const propsRef = toRefs(props);
     const { stakingBalances, unbondingDelegations } = useAccount();
-    const displayPrice = computed(() => {
-      if (props.balances.length > 0) {
-        const liquidValue = (props.balances as Balances).reduce((total, balance) => {
+
+    const verifiedDenoms = computed(() => store.getters[GlobalDemerisGetterTypes.API.getVerifiedDenoms]);
+    const liquidValue = computed(() => {
+      if (propsRef.balances.value.length > 0) {
+        return (propsRef.balances.value as Balances).reduce((total, balance) => {
           if (balance.verified) {
             if (store.getters[GlobalDemerisGetterTypes.API.getPrice]({ denom: balance.base_denom })) {
               let totalValue =
@@ -57,74 +59,81 @@ export default defineComponent({
             return total;
           }
         }, 0);
-        const verifiedDenoms = store.getters[GlobalDemerisGetterTypes.API.getVerifiedDenoms];
-        const stakedValue = stakingBalances.value.reduce((total, stakingBalance) => {
-          const stakedDenom = verifiedDenoms.filter((x) => x.chain_name == stakingBalance.chain_name && x.stakable);
-          if (stakedDenom.length > 0) {
-            if (store.getters[GlobalDemerisGetterTypes.API.getPrice]({ denom: stakedDenom[0].name })) {
-              let totalValue =
-                parseInt(stakingBalance.amount) *
-                  store.getters[GlobalDemerisGetterTypes.API.getPrice]({ denom: stakedDenom[0].name }) ?? 0;
-              let precision = Math.pow(
-                10,
-                parseInt(
-                  store.getters[GlobalDemerisGetterTypes.API.getDenomPrecision]({
-                    name: stakedDenom[0].name,
-                  }) || 6,
-                ),
-              );
-              let value = totalValue / precision;
-              if (value) {
-                return total + value;
-              } else {
-                return total;
-              }
-            } else {
-              return total;
-            }
-          } else {
-            return total;
-          }
-        }, 0);
-        const unstakedValue = unbondingDelegations.value.reduce((total, unstakingBalance) => {
-          const unstakedDenom = verifiedDenoms.filter((x) => x.chain_name == unstakingBalance.chain_name && x.stakable);
-
-          if (unstakedDenom.length > 0) {
-            let unstakedAmount;
-            const unstakedAmounts = unstakingBalance.entries.map((z) => z.balance);
-            if (unstakedAmounts.length > 0) {
-              unstakedAmount = unstakedAmounts.reduce((acc, item) => +parseInt(item) + acc, 0);
-            }
-            if (store.getters[GlobalDemerisGetterTypes.API.getPrice]({ denom: unstakedDenom[0].name })) {
-              let totalValue =
-                parseInt(unstakedAmount) *
-                  store.getters[GlobalDemerisGetterTypes.API.getPrice]({ denom: unstakedDenom[0].name }) ?? 0;
-              let precision = Math.pow(
-                10,
-                parseInt(
-                  store.getters[GlobalDemerisGetterTypes.API.getDenomPrecision]({
-                    name: unstakedDenom[0].name,
-                  }) || 6,
-                ),
-              );
-              let value = totalValue / precision;
-              if (value) {
-                return total + value;
-              } else {
-                return total;
-              }
-            } else {
-              return total;
-            }
-          } else {
-            return total;
-          }
-        }, 0);
-        const value = liquidValue + stakedValue + unstakedValue;
-        return Number.isFinite(value) ? value : 0;
       } else {
         return 0;
       }
+    });
+    const stakedValue = computed(() => {
+      return stakingBalances.value.reduce((total, stakingBalance) => {
+        const stakedDenom = verifiedDenoms.value.filter((x) => x.chain_name == stakingBalance.chain_name && x.stakable);
+        if (stakedDenom.length > 0) {
+          if (store.getters[GlobalDemerisGetterTypes.API.getPrice]({ denom: stakedDenom[0].name })) {
+            let totalValue =
+              parseInt(stakingBalance.amount) *
+                store.getters[GlobalDemerisGetterTypes.API.getPrice]({ denom: stakedDenom[0].name }) ?? 0;
+            let precision = Math.pow(
+              10,
+              parseInt(
+                store.getters[GlobalDemerisGetterTypes.API.getDenomPrecision]({
+                  name: stakedDenom[0].name,
+                }) || 6,
+              ),
+            );
+            let value = totalValue / precision;
+            if (value) {
+              return total + value;
+            } else {
+              return total;
+            }
+          } else {
+            return total;
+          }
+        } else {
+          return total;
+        }
+      }, 0);
+    });
+    const unstakingValue = computed(() => {
+      return unbondingDelegations.value.reduce((total, unstakingBalance) => {
+        const unstakedDenom = verifiedDenoms.value.filter(
+          (x) => x.chain_name == unstakingBalance.chain_name && x.stakable,
+        );
+
+        if (unstakedDenom.length > 0) {
+          let unstakedAmount;
+          const unstakedAmounts = unstakingBalance.entries.map((z) => z.balance);
+          if (unstakedAmounts.length > 0) {
+            unstakedAmount = unstakedAmounts.reduce((acc, item) => +parseInt(item) + acc, 0);
+          }
+          if (store.getters[GlobalDemerisGetterTypes.API.getPrice]({ denom: unstakedDenom[0].name })) {
+            let totalValue =
+              parseInt(unstakedAmount) *
+                store.getters[GlobalDemerisGetterTypes.API.getPrice]({ denom: unstakedDenom[0].name }) ?? 0;
+            let precision = Math.pow(
+              10,
+              parseInt(
+                store.getters[GlobalDemerisGetterTypes.API.getDenomPrecision]({
+                  name: unstakedDenom[0].name,
+                }) || 6,
+              ),
+            );
+            let value = totalValue / precision;
+            if (value) {
+              return total + value;
+            } else {
+              return total;
+            }
+          } else {
+            return total;
+          }
+        } else {
+          return total;
+        }
+      }, 0);
+    });
+    const displayPrice = computed(() => {
+      const value = liquidValue.value + stakedValue.value + unstakingValue.value;
+      return Number.isFinite(value) ? value : 0;
     });
     return { displayPrice };
   },
