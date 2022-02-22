b878fd7f8d90177f3d00c1d790e0df741462660e
Clockwork <alex@clockwork.gr>
2022-02-22 10:49:51 +0200

    feat: Add loaders to pool pages

--- src/views/Pools.vue
+++ src/views/Pools.vue
@@ -5,6 +5,7 @@
     </header>
 
     <PoolsTable v-if="pools" :pools="pools" />
+    <SkeletonLoader v-else width="100%" height="300px" />
   </AppLayout>
 </template>
 
@@ -13,6 +14,7 @@ import { computed } from '@vue/reactivity';
 import { useI18n } from 'vue-i18n';
 import { useMeta } from 'vue-meta';
 
+import SkeletonLoader from '@/components/common/loaders/SkeletonLoader.vue';
 import PoolsTable from '@/components/liquidity/PoolsTable.vue';
 import usePools from '@/composables/usePools';
 import AppLayout from '@/layouts/AppLayout.vue';
@@ -20,7 +22,7 @@ import { pageview } from '@/utils/analytics';
 
 export default {
   name: 'Pools',
-  components: { AppLayout, PoolsTable },
+  components: { AppLayout, PoolsTable, SkeletonLoader },
 
   setup() {
     const { t } = useI18n({ useScope: 'global' });
