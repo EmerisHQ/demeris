7268b8e0222241e7de8ad2f090f2e02be48bb2e1
Jesse Eilers <eitjuh@gmail.com>
2022-02-25 16:43:59 +0100

    adding sentry

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import './assets/scss/index.scss';
 import 'tippy.js/dist/tippy.css';
 
+import { BrowserTracing } from '@sentry/tracing';
+import * as Sentry from '@sentry/vue';
 import vueLib from '@starport/vue';
 import mitt from 'mitt';
 import { createPinia } from 'pinia';
@@ -37,9 +39,26 @@ const i18n = createI18n({
 const emitter = mitt();
 
 const app = createApp(App);
+
 app.config.globalProperties.emitter = emitter;
 app.config.globalProperties._depsLoaded = true;
 app.use(VueApexCharts);
+
+Sentry.init({
+  app,
+  dsn: 'https://062027a7ae3c4e85b35fed27465f9615@o1152630.ingest.sentry.io/6232236',
+  integrations: [
+    new BrowserTracing({
+      routingInstrumentation: Sentry.vueRouterInstrumentation(router),
+      tracingOrigins: ['localhost', 'app.emeris.com', /^\//],
+    }),
+  ],
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: parseFloat(process.env.VUE_APP_SENTRY_TRACES_SAMPLE_RATE),
+});
+
 app
   .use(i18n)
   .use(store)
