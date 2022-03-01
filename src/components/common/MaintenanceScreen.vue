4ded82a44213fb1f699135bf9326ebedda905630
cauduro.dev <60057887+caudurodev@users.noreply.github.com>
2022-03-01 15:25:52 +0100

    Add fixes/refactor to maintenance page

--- src/components/common/MaintenanceScreen.vue
+++ src/components/common/MaintenanceScreen.vue
@@ -1,34 +1,28 @@
 <template>
-  <div class=" w-full h-full m-0 p-0 z-50 text-text">
-    <div class="mt-7 mb-24 flex justify-center">
-      <img v-if="!isDarkMode" :src="logo" />
+  <div class="w-full h-screen flex flex-col dark:bg-red">
+    <div class="flex justify-center mt-7 mb-8">
+      <img v-if="!isBrowserDarkMode" :src="logo" />
       <img v-else :src="logoDark" />
     </div>
-    <div class="w-full mb-16 flex justify-center">
-      <img :src="surfer" />
-    </div>
-    <div class="mb-48 w-full text-center flex justify-center">
-      <div class="custom-width">
-        <h1 class="font-bold text-3 mb-4">
-          {{ $t('maintenance.title') }}
-        </h1>
-        <p class="text-text opacity-60">
-          {{ $t('maintenance.subTitle') }}
-        </p>
+    <div class="w-full flex-1 pb-16">
+      <div class="flex flex-col  justify-center w-full h-full">
+        <div class="mb-16 w-full text-center flex justify-center">
+          <img :src="surfer" />
+        </div>
+        <div class="w-full text-center flex justify-center mb-8">
+          <div class="custom-width">
+            <h1 class="font-bold text-3 mb-4">{{ $t('maintenance.title') }}</h1>
+            <p class="text-text opacity-60">{{ $t('maintenance.subTitle') }}</p>
+          </div>
+        </div>
       </div>
     </div>
-    <div class="w-full flex items-center flex justify-center text-link text-0 mb-32">
-      <a class="mr-6 align-middle footer-link" href="https://t.me/EmerisHQ">
-        <Icon class="text-text opacity-30 gray-icon mr-2" name="LinkIcon" :icon-size="0.813" />
-        telegram&#8599;
-      </a>
-      <a class="mr-6 footer-link" href="https://medium.com/emeris-blog">
-        <Icon class="text-text opacity-30 gray-icon  mr-2" name="LinkIcon" :icon-size="0.813" /> 
-        medium&#8599;
-      </a>
-      <a class="mr-6 footer-link" href="https://twitter.com/emerisHQ">
-        <Icon class="text-text opacity-30 gray-icon  mr-2" name="LinkIcon" :icon-size="0.813" /> 
-        twitter&#8599;
+    <div class="flex w-full items-center justify-center text-link h-32 pb-32">
+      <a v-for="{url,label} in footerLinks" :key="url" class="mr-6 align-middle -text-1" :href="url">
+        <span class="inline-flex">
+          <Icon class="text-text opacity-30 mr-2 dark:opacity-1" name="LinkIcon" :icon-size="0.813" />
+        </span>
+        {{ label }}&#8599;
       </a>
     </div>
   </div>
@@ -36,13 +30,19 @@
 
 <script lang="ts" setup>
 import { useDark } from '@vueuse/core'
+import { ref }from 'vue'
 
 import logo from "@/assets/images/maintenance-logo.png"
 import logoDark from "@/assets/images/maintenance-logo-dark.png"
 import surfer from "@/assets/images/maintenance-surfer.png"
-import Icon from '@/components/ui/Icon.vue'
+import Icon from '@/components/ui/Icon.vue';
 
-const isDarkMode = useDark()
+const isBrowserDarkMode = useDark()
+const footerLinks = ref([
+  {label:'telegram',url:'https://t.me/EmerisHQ'},
+  {label:'medium',url:'https://medium.com/emeris-blog'},
+  {label:'twitter',url:'https://twitter.com/emerisHQ'},
+])
 </script>
 
 <style>
