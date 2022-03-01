b4a8884a918ac33fe38ebc16d3f62795a8fc1b99
Jesse <jesse.eilers@tendermint.com>
2022-03-01 14:14:07 +0100

    Add maintenance screen (#1185)
    Co-authored-by: cauduro.dev <60057887+caudurodev@users.noreply.github.com>

---
+++ src/components/common/MaintenanceScreen.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class=" w-full h-full m-0 p-0 z-50 text-text">
+    <div class="mt-7 mb-24 flex justify-center">
+      <img v-if="!isDarkMode" :src="logo" />
+      <img v-else :src="logoDark" />
+    </div>
+    <div class="w-full mb-16 flex justify-center">
+      <img :src="surfer" />
+    </div>
+    <div class="mb-48 w-full text-center flex justify-center">
+      <div class="custom-width">
+        <h1 class="font-bold text-3 mb-4">
+          {{ $t('maintenance.title') }}
+        </h1>
+        <p class="text-text opacity-60">
+          {{ $t('maintenance.subTitle') }}
+        </p>
+      </div>
+    </div>
+    <div class="w-full flex items-center flex justify-center text-link text-0 mb-32">
+      <a class="mr-6 align-middle footer-link" href="https://t.me/EmerisHQ">
+        <Icon class="text-text opacity-30 gray-icon mr-2" name="LinkIcon" :icon-size="0.813" />
+        telegram&#8599;
+      </a>
+      <a class="mr-6 footer-link" href="https://medium.com/emeris-blog">
+        <Icon class="text-text opacity-30 gray-icon  mr-2" name="LinkIcon" :icon-size="0.813" /> 
+        medium&#8599;
+      </a>
+      <a class="mr-6 footer-link" href="https://twitter.com/emerisHQ">
+        <Icon class="text-text opacity-30 gray-icon  mr-2" name="LinkIcon" :icon-size="0.813" /> 
+        twitter&#8599;
+      </a>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { useDark } from '@vueuse/core'
+
+import logo from "@/assets/images/maintenance-logo.png"
+import logoDark from "@/assets/images/maintenance-logo-dark.png"
+import surfer from "@/assets/images/maintenance-surfer.png"
+import Icon from '@/components/ui/Icon.vue'
+
+const isDarkMode = useDark()
+</script>
+
+<style>
+.custom-width{
+  width: 495px;
+}
+.footer-link{
+ font-size: .813rem !important;
+}
+.gray-icon{
+  display: inline-block !important;
+}
+</style>
