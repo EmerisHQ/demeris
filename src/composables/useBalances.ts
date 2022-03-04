484a1f466da672f55614ce320a674eb48abea54a
Fabian Weber <fabian.weber@tendermint.com>
2022-03-04 18:11:44 +0530

    working all balances

---
+++ src/composables/useBalances.ts
@@ -0,0 +1,153 @@
+import { chainAddressfromKeyhash } from '../utils/basic';
+
+export default function useBalances() {
+  const getTrace = async (ibcDenom, chainConfig) => {
+    const hash = ibcDenom.substring(4);
+    const { denom_trace } = await fetch(chainConfig.apiUrl + `/ibc/apps/transfer/v1/denom_traces/${hash}`).then((res) =>
+      res.json(),
+    );
+    return denom_trace;
+  };
+
+  const chainConfigs = {
+    'cosmos-hub-4': {
+      prefix: 'cosmos',
+      // rpcUrl: 'https://rpc.cosmos.network',
+      stakingDenom: 'uatom',
+      apiUrl: 'https://api.cosmos.network',
+      // apiUrl: 'https://cosmos-hub-emeris.app.alpha.starport.cloud'
+    },
+    // 'akash-testnet': {
+    //     prefix: 'akash',
+    //     // rpcUrl: 'https://rpc.cosmos.network',
+    //     apiUrl: 'https://api.akash-emeris.app.alpha.starport.cloud/'
+    // }
+  };
+  const getChainConfig = (chainId) => {
+    const chainConfig = chainConfigs[chainId];
+    if (!chainConfig) throw new Error('Cant trace ibc denom as there is no config for chain' + chainId);
+    return chainConfig;
+  };
+  const getIbcTokenChain = async (ibcDenom, chainConfig) => {
+    const trace = await getTrace(ibcDenom, chainConfig);
+    console.log(trace.path);
+    const chainId = await getIbcTokenChainFromPath(trace.path, chainConfig);
+    return { baseDenom: trace.base_denom, chainId };
+  };
+  const getIbcTokenChainFromPath = async (path, chainConfig) => {
+    // const ibcClient = (await getChainClients(chainConfig)).ibcClient
+    const splitDenomTrace = path.split('/');
+    const { identified_client_state } = await fetch(
+      chainConfig.apiUrl +
+        `/ibc/core/channel/v1/channels/${splitDenomTrace[1]}/ports/${splitDenomTrace[0]}/client_state`,
+    ).then((res) => res.json());
+    // if the trace is longer then 2 there is more then 1 hop so we do a recursive search
+    if (splitDenomTrace.length > 2) {
+      return getIbcTokenChainFromPath(
+        splitDenomTrace.slice(2).join('/'),
+        getChainConfig(identified_client_state.clientState.chain_id),
+      );
+    } else {
+      return identified_client_state.client_state.chain_id;
+    }
+  };
+  const getDelegations = async (address, chainConfig) => {
+    const { delegation_responses } = await fetch(
+      chainConfig.apiUrl + `/cosmos/staking/v1beta1/delegations/${address}`,
+    ).then((res) => res.json());
+    return delegation_responses.map(({ balance }) => balance);
+  };
+  const getUndelegations = async (address, chainConfig) => {
+    const { unbonding_responses } = await fetch(
+      chainConfig.apiUrl + `/cosmos/staking/v1beta1/delegators/${address}/unbonding_delegations`,
+    ).then((res) => res.json());
+    return [
+      ...unbonding_responses.reduce((all, { entries }) => {
+        return all.concat(
+          ...entries.map((entry) => ({ denom: chainConfig.stakingDenom, amount: Number(entry.balance) })),
+        );
+      }, []),
+    ];
+  };
+  const getRedelegations = async (address, chainConfig) => {
+    const { redelegation_responses } = await fetch(
+      chainConfig.apiUrl + `/cosmos/staking/v1beta1/delegators/${address}/redelegations`,
+    ).then((res) => res.json());
+    return [
+      ...redelegation_responses.map(({ entries }) =>
+        entries.map((entry) => ({ denom: chainConfig.stakingDenom, amount: Number(entry.balance) })),
+      ),
+    ];
+  };
+  const getStakingBalances = async (address, chainConfig) => {
+    const [delegations, undelegations, redelegations] = await Promise.all([
+      getDelegations(address, chainConfig),
+      getUndelegations(address, chainConfig),
+      getRedelegations(address, chainConfig),
+    ]);
+    return [...delegations, ...undelegations, ...redelegations];
+  };
+  const aggregateBalances = (balances) => {
+    const allBalances = {};
+    balances.forEach(({ denom, amount }) => {
+      allBalances[denom] = (allBalances[denom] || 0) + Number(amount);
+    });
+    return Object.entries(allBalances).map(([key, value]) => ({ denom: key, amount: value }));
+  };
+  const getBalances = async (keyHash) => {
+    // const { pools } = await fetch(process.env.VUE_APP_EMERIS_PROD_LIQUIDITY_ENDPOINT + '/cosmos/liquidity/v1beta1/pools').then(res => res.json())
+    console.time('all');
+    await Promise.all(
+      Object.values(chainConfigs).map(async (chainConfig) => {
+        console.time(chainConfig.prefix);
+        const address = chainAddressfromKeyhash(chainConfig.prefix, keyHash); // 'cosmos1ttm76wws8lct6ua09c2jz463jweelhy2c0m6r0'
+        // const { client } = await getChainClients(chainConfig)
+        // const balances = await client.getAllBalances(address)
+        const [{ balances }, stakingBalances] = await Promise.all([
+          fetch(chainConfig.apiUrl + `/cosmos/bank/v1beta1/balances/${address}`).then((res) => res.json()),
+          getStakingBalances(address, chainConfig),
+        ]);
+        const allBalances = aggregateBalances([...stakingBalances, ...balances]);
+
+        const ibcTraces = await Promise.all(
+          allBalances
+            .filter((balance) => balance.denom.startsWith('ibc/'))
+            .map(async (ibcBalance) => {
+              return {
+                ibcDenom: ibcBalance.denom,
+                ...(await getIbcTokenChain(ibcBalance.denom, chainConfig)),
+              };
+            }),
+        );
+        const resolvedBalances = allBalances.map((balance) => {
+          if (balance.denom.startsWith('ibc/')) {
+            // @ts-ignore
+            const { chainId, baseDenom } = ibcTraces.find((trace) => trace.ibcDenom === balance.denom);
+            return {
+              ...balance,
+              baseDenom,
+              chainId,
+            };
+          }
+          // if (balance.denom.startsWith('pool')) {
+          //     const pool = pools.find(({ pool_coin_denom }) => pool_coin_denom === balance.denom)
+          //     return {
+          //         ...balance,
+          //         pool
+          //     }
+          // }
+          return balance;
+        });
+        console.log(chainConfig.prefix, resolvedBalances);
+        console.timeEnd(chainConfig.prefix);
+      }),
+    );
+    console.timeEnd('all');
+  };
+
+  return {
+    getBalances,
+  };
+}
+
+useBalances().getBalances('a9291de7ab7e6e6559f889ed326ca2c0859165a3');
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import './assets/scss/index.scss';
 import 'tippy.js/dist/tippy.css';
+import './composables/useBalances';
 
 import vueLib from '@starport/vue';
 import mitt from 'mitt';
@@ -18,46 +19,46 @@ import App from './App.vue';
 import router from './router';
 import { store } from './store/setup';
 
-const i18n = createI18n({
-  globalInjection: true,
-  legacy: false,
-  locale: 'en',
-  fallbackLocale: 'en',
-  messages,
-  numberFormats: {
-    en: {
-      currency: {
-        style: 'currency',
-        currency: 'USD',
-        notation: 'standard',
-      },
-    },
-  },
-});
-const emitter = mitt();
+// const i18n = createI18n({
+//   globalInjection: true,
+//   legacy: false,
+//   locale: 'en',
+//   fallbackLocale: 'en',
+//   messages,
+//   numberFormats: {
+//     en: {
+//       currency: {
+//         style: 'currency',
+//         currency: 'USD',
+//         notation: 'standard',
+//       },
+//     },
+//   },
+// });
+// const emitter = mitt();
 
-const app = createApp(App);
-app.config.globalProperties.emitter = emitter;
-app.config.globalProperties._depsLoaded = true;
-app.use(VueApexCharts);
-app
-  .use(i18n)
-  .use(store)
-  .use(router)
-  .use(vueLib)
-  .use(VueTippy)
-  .use(VueCookieNext)
-  .use(createMetaManager())
-  .use(createPinia())
-  .use(VueGtag, {
-    config: {
-      id: 'UA-201374903-1',
-      params: {
-        anonymize_ip: true,
-        send_page_view: false,
-      },
-    },
-  })
-  .mount('#app');
+// const app = createApp(App);
+// app.config.globalProperties.emitter = emitter;
+// app.config.globalProperties._depsLoaded = true;
+// app.use(VueApexCharts);
+// app
+//   .use(i18n)
+//   .use(store)
+//   .use(router)
+//   .use(vueLib)
+//   .use(VueTippy)
+//   .use(VueCookieNext)
+//   .use(createMetaManager())
+//   .use(createPinia())
+//   .use(VueGtag, {
+//     config: {
+//       id: 'UA-201374903-1',
+//       params: {
+//         anonymize_ip: true,
+//         send_page_view: false,
+//       },
+//     },
+//   })
+//   .mount('#app');
 
-VueCookieNext.config({ expire: '180d', domain: '.emeris.com' });
+// VueCookieNext.config({ expire: '180d', domain: '.emeris.com' });
