1fa4aa88ac55103587493063747d0f125a367847
caudurodev <caudurodev@caudurodevs-MacBook-Pro.local>
2022-03-09 14:48:55 +0100

    Remove unused fields from interface

--- src/components/common/ValidatorImg.vue
+++ src/components/common/ValidatorImg.vue
@@ -14,25 +14,7 @@ import { ref, toRefs } from 'vue';
 
 import { getFirstAlphabet } from '@/utils/basic';
 interface Validator {
-  chain_name?: string;
-  operator_address?: string;
-  consensus_pubkey_type?: string;
-  consensus_pubkey_value?: string;
-  jailed?: boolean;
-  status?: number;
-  tokens?: string;
-  delegator_shares?: string;
   moniker: string;
-  identity?: string;
-  website?: string;
-  details?: string;
-  unbonding_height?: number;
-  unbonding_time?: string;
-  commission_rate?: string;
-  max_rate?: string;
-  max_change_rate?: string;
-  update_time?: string;
-  min_self_delegation?: number;
   avatar?: string;
 }
 interface Props {
