c09b2aac22b73f55133d353b31b40369346dfbfc
Raymahni <busram2009@gmail.com>
2022-03-16 00:50:09 +0100

    enabled dark mode for loading icon and fixed keyword warning on console

--- src/components/common/Icons/LoadingIcon.vue
+++ src/components/common/Icons/LoadingIcon.vue
@@ -2,7 +2,7 @@
   <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path
       d="M15 8C15 4.13401 11.866 1 8 1C4.13401 1 1 4.13401 1 8C1 11.866 4.13401 15 8 15"
-      stroke="black"
+      stroke="currentColor"
       stroke-width="1.6"
       stroke-miterlimit="10"
       stroke-linecap="round"
