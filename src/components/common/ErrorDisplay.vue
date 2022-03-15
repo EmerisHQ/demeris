a244af4e27b56ef75b51e5c338f8fb70d701101c
fl-y <thedigitalimg@gmail.com>
2022-03-15 20:42:52 +0900

    fix: cleanup comments and resolve merge conflicts

--- src/components/common/ErrorDisplay.vue
+++ src/components/common/ErrorDisplay.vue
@@ -26,44 +26,4 @@ const props = withDefaults(defineProps<ErrorPanelProps>(), {
   msg: "We couldn't load your data.",
   userActionMsg: 'Please wait.',
 });
-/*
-export default defineComponent({
-
-  props: {
-    error: {
-      type: [Object, String],
-      required: true,
-    },
-  },
-  computed: {
-    title(): string {
-      if (typeof this.error === 'object') {
-        const error = this.error as GenericFetchError;
-        if (error.response?.status === 404) {
-          return 'Not Found';
-        } else if (error?.message === 'Failed to fetch') {
-          return 'Network Error';
-        }
-      }
-      return 'Something gone wrong';
-    },
-    message(): string {
-      if (typeof this.error === 'string') {
-        return this.error;
-      }
-      const error = this.error as GenericFetchError;
-      if (error.response?.status === 404) {
-        return 'Sorry, the asked city was not found.';
-      } else if (error.message === 'Failed to fetch') {
-        return 'Sorry, we was not able to perform the request.';
-      }
-      return error.message;
-    },
-    isTryAgain(): boolean {
-      const error = this.error as GenericFetchError;
-      return !error.response || error.response.status !== 404;
-    },
-  },
-});
-*/
 </script>
