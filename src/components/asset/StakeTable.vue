6b0a5e293832e50562493f3a6cc856eeaeeb0e2a
Jesse <jesse.eilers@tendermint.com>
2022-03-18 15:25:03 +0100

    fix: Fixes staking banners on the left side, to always show if an asset is stakable (#1357)
    Co-authored-by: Alex Megalokonomos <alex@clockwork.gr>

--- src/components/asset/StakeTable.vue
+++ src/components/asset/StakeTable.vue
@@ -9,15 +9,18 @@
     <AsyncBoundary width="100%" height="300px">
       <StakeTableContents :selected-tab="selectedTab" :denom="props.denom" :total-rewards-amount="totalRewardsAmount" />
     </AsyncBoundary>
+    <StakeTableBanner v-if="showStakingBanner" :denom="props.denom" />
   </div>
 </template>
 <script lang="tsx" setup>
 import { computed, ref, watch } from 'vue';
 import { useStore } from 'vuex';
 
+import StakeTableBanner from '@/components/asset/StakeTableBanner.vue';
 import StakeTableContents from '@/components/asset/StakeTableContents.vue';
 import StakeTableTitle from '@/components/asset/StakeTableTitle.vue';
 import AsyncBoundary from '@/components/common/AsyncBoundary.vue';
+import useAccount from '@/composables/useAccount';
 import useStaking, { StakingRewards } from '@/composables/useStaking';
 import { GlobalGetterTypes, RootStoreTyped } from '@/store';
 
@@ -26,6 +29,16 @@ const stakingRewardsData = ref<StakingRewards>(null);
 const selectedTab = ref<number>(1);
 const props = defineProps<{ denom: string }>();
 const { getStakingRewardsByBaseDenom } = useStaking();
+const { stakingBalancesByChain } = useAccount();
+const stakingBalances = computed(() => {
+  return stakingBalancesByChain(
+    store.getters[GlobalGetterTypes.API.getChainNameByBaseDenom]({ denom: props.denom }),
+  ).filter((x) => Math.floor(parseFloat(x.amount)) > 0);
+});
+
+const showStakingBanner = computed(() => {
+  return stakingBalances.value.length === 0;
+});
 
 const isSignedIn = computed(() => {
   return store.getters[GlobalGetterTypes.USER.isSignedIn];
