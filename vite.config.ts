06e1c528be3cecf6d8fc17aaa3c3ef19705d96b0
caudurodev <rod@cauduro.dev>
2022-03-21 11:15:10 +0100

    Disable sourcemap in vite and fix workflow indent

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,7 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   build: {
     chunkSizeWarningLimit: 1500,
+    sourcemap: false,
   },
   plugins: [vue(), nodeResolve(), envCompatible(), dynamicImport()],
   resolve: {
