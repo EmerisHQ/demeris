28a7833e5c2ad803d2835ecb0c4b71e2eeac512e
Alex Megalokonomos <alex@clockwork.gr>
2022-03-21 16:03:52 +0200

    chore: Remove cypress from project (#1385)

--- cypress/support/sub-pages-paths.js
+++
@@ -1,8 +0,0 @@
-export class SubPagesPaths {
-  assets_path = '/assets';
-  pools_path = '/pools';
-  receive_path = '/receive';
-  send_path = '/send';
-  send_to_adress_path = '/send/address';
-  send_move_assets_path = '/send/move';
-}
