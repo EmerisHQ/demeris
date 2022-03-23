d7ae8df5efe36b204d318096a9940be5f526d8cc
Jesse <jesse.eilers@tendermint.com>
2022-03-23 10:33:01 +0100

    fix/playwright-3-retries (#1412)

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,7 +24,7 @@ const config: PlaywrightTestConfig = {
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: 1,
+  retries: 3,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 4 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
