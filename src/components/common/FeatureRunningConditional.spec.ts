df97d07264e6841d4c74e5cd814afa0f6ec0ab09
Clockwork <alex@clockwork.gr>
2022-03-23 23:44:10 +0200

    fix: Fix env-dependent test file

--- src/components/common/FeatureRunningConditional.spec.ts
+++ src/components/common/FeatureRunningConditional.spec.ts
@@ -4,29 +4,20 @@ import { mount } from '@vue/test-utils';
 import { loadFeaturesRunning } from '../../utils/FeatureManager';
 import FeatureRunningConditional from './FeatureRunningConditional.vue';
 
-const env = process.env;
-
-afterEach(() => {
-  process.env = env;
-});
-console.log(process.env);
 test('should render with env when activated', () => {
-  process.env.VITE_FEATURE_MY_TEST = 'true';
   loadFeaturesRunning();
-
   const wrapper = mount(FeatureRunningConditional, {
-    props: { name: 'MY_TEST' },
+    props: { name: 'MY_ENABLED_FEATURE' },
     slots: { default: '<h1>Enabled</h1>' },
   });
   expect(wrapper.html()).toBe('<h1>Enabled</h1>');
 });
 
 test('should render with env when deactivated', () => {
-  process.env.VITE_FEATURE_MY_TEST = 'false';
   loadFeaturesRunning();
 
   const wrapper = mount(FeatureRunningConditional, {
-    props: { name: 'MY_TEST' },
+    props: { name: 'MY_DISABLED_FEATURE' },
     slots: { deactivated: '<h1>Disabled</h1>' },
   });
   expect(wrapper.html()).toBe('<h1>Disabled</h1>');
