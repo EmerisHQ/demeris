65ab0c761b3faab360751dc4c7e49eebe915e000
fl-y <thedigitalimg@gmail.com>
2022-04-11 15:55:58 +0900

    fix: switch implementation to regex

--- src/utils/api-settings.ts
+++ src/utils/api-settings.ts
@@ -4,8 +4,9 @@ export function axiosInit() {
   axios.interceptors.request.use(axiosIntercepter);
 }
 
+const falsyRegex = /(undefined|null)+/;
 const axiosIntercepter = (config: AxiosRequestConfig) => {
-  if (config.url.includes('undefined') || config.url.includes('null') || config.url === undefined) {
+  if (falsyRegex.test(config.url) || config.url === undefined) {
     throw new Error(`Request url includes undefined or null - [${config.url}]`);
   }
   return config;
