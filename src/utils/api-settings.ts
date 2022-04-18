83fd09bc2440c80f688d87098c1d478f59efe117
fl-y <thedigitalimg@gmail.com>
2022-04-18 20:17:06 +0900

    fix: also catch when chainnames are undefined

--- src/utils/api-settings.ts
+++ src/utils/api-settings.ts
@@ -4,7 +4,7 @@ export function axiosInit() {
   axios.interceptors.request.use(axiosIntercepter);
 }
 
-const falsyRegex = /(=undefined|=null)+/;
+const falsyRegex = /([=/]+undefined|[=/]+null)+/;
 const axiosIntercepter = (config: AxiosRequestConfig) => {
   if (falsyRegex.test(config.url) || config.url === undefined) {
     throw new Error(`Request url includes undefined or null - [${config.url}]`);
