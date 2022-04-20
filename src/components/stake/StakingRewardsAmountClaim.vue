52dffad5d3620dab85b7280fee6cbd31d4784e7f
Dawntraoz <albasilventefu@gmail.com>
2022-04-20 14:27:28 +0200

    refactor: add the totalRewards amount inside the staking composable

--- src/components/stake/StakingRewardsAmountClaim.vue
+++ src/components/stake/StakingRewardsAmountClaim.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex items-center justify-end gap-x-4">
     <Price
-      :amount="{ denom: denom, amount: totalRewardsAmount + '' }"
+      :amount="{ denom: denom, amount: `${totalRewardsAmount}` }"
       :show-dash="false"
       :label="label"
       :class="{ 'text-muted': totalRewardsAmount <= 0 }"
@@ -13,34 +13,12 @@
 </template>
 
 <script lang="ts" setup>
-import { computed, ref, watch } from 'vue';
-import { useStore } from 'vuex';
-
 import Price from '@/components/common/Price.vue';
 import Button from '@/components/ui/Button.vue';
-import useStaking, { StakingRewards } from '@/composables/useStaking';
-import { GlobalGetterTypes, RootStoreTyped } from '@/store';
-import { getSumOfRewards } from '@/utils/basic';
-const store = useStore() as RootStoreTyped;
-const { getStakingRewardsByBaseDenom } = useStaking();
+import useStaking from '@/composables/useStaking';
 
 const props = defineProps<{ denom: string; label: string }>();
 
-const stakingRewardsData = ref<StakingRewards>(null);
-
-const isSignedIn = computed(() => {
-  return store.getters[GlobalGetterTypes.USER.isSignedIn];
-});
-
-const totalRewardsAmount = computed(() => {
-  return getSumOfRewards(stakingRewardsData.value?.total, props.denom);
-});
-
-watch(
-  () => isSignedIn.value,
-  async () => {
-    stakingRewardsData.value = await getStakingRewardsByBaseDenom(props.denom);
-  },
-  { immediate: true },
-);
+const { getTotalRewardsAmount } = useStaking();
+const totalRewardsAmount = getTotalRewardsAmount(props.denom);
 </script>
