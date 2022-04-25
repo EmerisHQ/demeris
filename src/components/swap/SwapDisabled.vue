80360c2601ea1c614d95f69ef11cad76bcd0fad5
fl-y <thedigitalimg@gmail.com>
2022-04-26 03:49:14 +0900

    fix: remove subscribe cta

--- src/components/swap/SwapDisabled.vue
+++ src/components/swap/SwapDisabled.vue
@@ -1,16 +1,13 @@
 <template>
   <section class="w-full rounded-xl h-[200px] bg-fg p-6 flex flex-col justify-between">
-    <div>
-      <h4 class="text-1 font-medium mb-3.5">{{ $t('components.swap.disabledTitle') }}</h4>
-      <a
-        href="https://support.emeris.com/en/articles/6095013-how-to-withdraw-your-funds-from-liquidity-pools"
-        class="font-[500]"
-        target="_blank"
-        rel="noopener noreferrer"
-        >{{ $t('components.swap.disabledLearnMore') }}</a
-      >
-    </div>
-    <p class="text-muted -text-1">{{ $t('components.swap.disabledCTA') }}</p>
+    <h4 class="text-1 font-medium mb-3.5">{{ $t('components.swap.disabledTitle') }}</h4>
+    <a
+      href="https://support.emeris.com/en/articles/6095013-how-to-withdraw-your-funds-from-liquidity-pools"
+      class="font-[500]"
+      target="_blank"
+      rel="noopener noreferrer"
+      >{{ $t('components.swap.disabledLearnMore') }}</a
+    >
   </section>
 </template>
 
