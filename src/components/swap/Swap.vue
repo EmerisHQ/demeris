7d9d60ebe3f265b32544b6926af4e8389e657078
Joon <thedigitalimg@gmail.com>
2022-04-26 15:49:11 +0900

    feat: add SwapDisabled component and apply (#1660)
    * feat: add SwapDisabled component and apply
    
    * fix: remove subscribe cta

---
+++ src/components/swap/Swap.vue
@@ -0,0 +1,29 @@
+<template>
+  <FeatureRunningConditional name="SWAP_WIDGET_DISABLED">
+    <template #deactivated>
+      <FeatureRunningConditional name="DEX_AGG">
+        <template #deactivated>
+          <LiquiditySwap :default-asset="nativeAsset" />
+        </template>
+        <DexSwap :default-asset="nativeAsset" />
+      </FeatureRunningConditional>
+    </template>
+    <SwapDisabled />
+  </FeatureRunningConditional>
+</template>
+
+<script setup lang="ts">
+import { EmerisAPI } from '@emeris/types';
+import { defineProps, toRefs } from 'vue';
+
+import FeatureRunningConditional from '@/components/common/FeatureRunningConditional.vue';
+import LiquiditySwap from '@/components/liquidity/Swap.vue';
+import DexSwap from '@/components/swap/DexSwap.vue';
+import SwapDisabled from '@/components/swap/SwapDisabled.vue';
+
+interface SwapProps {
+  nativeAsset?: EmerisAPI.Balance;
+}
+const props = defineProps<SwapProps>();
+const { nativeAsset } = toRefs(props);
+</script>
