645c10777bee0cac25a297b813ef7dbe7e50ae76
Joon <thedigitalimg@gmail.com>
2022-04-28 14:41:12 +0900

    Revert "fix: apply emergency switch for gdex sunset (#1663)"
    This reverts commit eb8796063ab554fe2532fe5f0e0942632db615cd.

--- e2e/pools.spec.ts
+++ e2e/pools.spec.ts
@@ -13,17 +13,17 @@ test.beforeEach(async ({ page, baseURL }) => {
   await expect(page).toHaveURL(baseURL + '/pools');
 });
 
-// test.describe('Pools location and availibility', function () {
-//   test('Pool search field', async ({ page }) => {
-//     const searchField = await page.locator("input[placeholder = 'Search assets and pools']");
-//     await searchField.click();
-//     await page.fill("input[placeholder = 'Search assets and pools']", 'test');
-//     await expect(searchField).toHaveValue('test');
-//   });
-//
-//   test('Add liquidity button usage', async ({ page, baseURL }) => {
-//     const addLiquidityButton = await page.locator('.add-liquidity', { hasText: 'Add liquidity' });
-//     await addLiquidityButton.click();
-//     await expect(page).toHaveURL(baseURL + '/pools/add');
-//   });
-// });
+test.describe('Pools location and availibility', function () {
+  test('Pool search field', async ({ page }) => {
+    const searchField = await page.locator("input[placeholder = 'Search assets and pools']");
+    await searchField.click();
+    await page.fill("input[placeholder = 'Search assets and pools']", 'test');
+    await expect(searchField).toHaveValue('test');
+  });
+
+  test('Add liquidity button usage', async ({ page, baseURL }) => {
+    const addLiquidityButton = await page.locator('.add-liquidity', { hasText: 'Add liquidity' });
+    await addLiquidityButton.click();
+    await expect(page).toHaveURL(baseURL + '/pools/add');
+  });
+});
