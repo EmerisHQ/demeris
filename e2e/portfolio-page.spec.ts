0188394102409f72232da1f38b70947fd3af0c69
Jesse Eilers <eitjuh@gmail.com>
2022-05-10 11:58:01 +0200

    test/fix-e2e-test-back-to-atom

--- e2e/portfolio-page.spec.ts
+++ e2e/portfolio-page.spec.ts
@@ -1,9 +1,13 @@
 import { expect, test } from '@playwright/test';
 
-import mockHandler from './mock-api';
+// import mockHandler from './mock-api';
 
 test.beforeEach(async ({ page, baseURL }) => {
-  page.route('https://api.emeris.com/**/*', mockHandler);
+  /**
+   * This test shouldn't be using any mocked data. It's a regression
+   * Also see: https://github.com/EmerisHQ/demeris/pull/1530
+   */
+  // page.route('https://api.emeris.com/**/*', mockHandler);
   page.on('domcontentloaded', () => {
     page.evaluate('window.Cypress=true; window.chrome=true; window.keplr={}');
   });
@@ -16,7 +20,7 @@ test.beforeEach(async ({ page, baseURL }) => {
   await expect(page).toHaveURL(baseURL + '/');
 });
 test.describe('Portfolio visual check', function () {
-  test('visibility of elements', async ({ page, baseURL }) => {
+  test('visibility of elements', async ({ page /*, baseURL*/ }) => {
     const totalBalance = await page.locator('text=Total balance');
     await expect(totalBalance).toBeVisible();
     const totalBalanceValue = await page.locator('*[class="total-price"]');
@@ -26,15 +30,21 @@ test.describe('Portfolio visual check', function () {
     await expect(pools).toBeVisible();
     const atomRow = await page.locator('table.assets-table').locator('tr', { hasText: 'ATOM' });
     await expect(atomRow).toBeVisible();
-    const dvpnPool = await page.locator('text=ATOM · DVPN');
-    await expect(dvpnPool).toBeVisible();
-    const irisPool = await page.locator('text=ATOM · IRIS');
-    await expect(irisPool).toBeVisible();
-    const osmoPool = await page.locator('text=ATOM · OSMO');
-    await expect(osmoPool).toBeVisible();
-    const irisDvpnPool = await page.locator('text=DVPN · IRIS');
-    await expect(irisDvpnPool).toBeVisible();
-    await irisDvpnPool.click();
-    await expect(page).toHaveURL(baseURL + '/pool/3');
+
+    /**
+     * Below commented out code was using mocked data.
+     * https://github.com/EmerisHQ/demeris/pull/1530
+     */
+
+    // const dvpnPool = await page.locator('text=ATOM · DVPN');
+    // await expect(dvpnPool).toBeVisible();
+    // const irisPool = await page.locator('text=ATOM · IRIS');
+    // await expect(irisPool).toBeVisible();
+    // const osmoPool = await page.locator('text=ATOM · OSMO');
+    // await expect(osmoPool).toBeVisible();
+    // const irisDvpnPool = await page.locator('text=DVPN · IRIS');
+    // await expect(irisDvpnPool).toBeVisible();
+    // await irisDvpnPool.click();
+    // await expect(page).toHaveURL(baseURL + '/pool/3');
   });
 });
