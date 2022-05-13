311f15cfbf1c46c00398ef7cf023e678b2dddf0c
Pranay Baldev <pranaybaldev@gmail.com>
2022-05-13 13:49:34 +0530

    argh

--- src/features/swap/components/SwapCoin/SwapCoin.vue
+++ src/features/swap/components/SwapCoin/SwapCoin.vue
@@ -28,7 +28,9 @@
           </div>
 
           <template v-if="isLoadingCoin || isLoadingChain">
-            <SkeletonLoader height="17px" width="96px" />
+            <div>
+              <SkeletonLoader height="17px" width="96px" />
+            </div>
           </template>
           <span v-else-if="chain" class="text-muted -text-1 whitespace-nowrap text-left">
             <ChainName :name="chain" />
