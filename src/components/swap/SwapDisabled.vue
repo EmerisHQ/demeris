8cbd801b75130aa8b15ed5a932fd275a4b6075af
Pranay Baldev <pranaybaldev@gmail.com>
2022-05-16 17:53:29 +0530

    update disabled swap component

--- src/components/swap/SwapDisabled.vue
+++ src/components/swap/SwapDisabled.vue
@@ -4,13 +4,9 @@
       <h4 class="text-1 font-medium mb-3.5">{{ $t('components.swap.disabledTitle') }}</h4>
       <p class="text-muted -text-1 leading-normal">{{ $t('components.swap.disabledMessage') }}</p>
     </div>
-    <a
-      href="https://support.emeris.com/en/articles/6095013-how-to-withdraw-your-funds-from-liquidity-pools"
-      class="font-[500]"
-      target="_blank"
-      rel="noopener noreferrer"
-      >{{ $t('components.swap.disabledLearnMore') }}</a
-    >
+    <a href="https://support.emeris.com/en/" class="font-[500]" target="_blank" rel="noopener noreferrer">{{
+      $t('components.swap.contactEmerisSupport')
+    }}</a>
   </section>
 </template>
 
