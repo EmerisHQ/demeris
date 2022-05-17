3047ca44ba0796086efb41b0839d46271c6f2604
Pranay Baldev <pranaybaldev@gmail.com>
2022-05-17 10:06:42 +0530

    update emeris support url

--- src/components/swap/SwapDisabled.vue
+++ src/components/swap/SwapDisabled.vue
@@ -4,9 +4,13 @@
       <h4 class="text-1 font-medium mb-3.5">{{ $t('components.swap.disabledTitle') }}</h4>
       <p class="text-muted -text-1 leading-normal">{{ $t('components.swap.disabledMessage') }}</p>
     </div>
-    <a href="https://support.emeris.com/en/" class="font-[500]" target="_blank" rel="noopener noreferrer">{{
-      $t('components.swap.contactEmerisSupport')
-    }}</a>
+    <a
+      href="https://support.emeris.com/en/collections/3445330-contact"
+      class="font-[500]"
+      target="_blank"
+      rel="noopener noreferrer"
+      >{{ $t('components.swap.contactEmerisSupport') }}</a
+    >
   </section>
 </template>
 
