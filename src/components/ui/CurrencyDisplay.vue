150143a066de4f18b727acafc79fffa8a74ab9dc
Dawntraoz <albasilventefu@gmail.com>
2022-05-19 14:07:58 +0200

    refactor: currency display to script setup

--- src/components/ui/CurrencyDisplay.vue
+++ src/components/ui/CurrencyDisplay.vue
@@ -52,58 +52,47 @@
   </i18n-n>
 </template>
 
-<script lang="ts">
+<script lang="ts" setup>
 import BigNumber from 'bignumber.js';
-import { computed, defineComponent, ref, watch } from 'vue';
+import { computed, ref, watch } from 'vue';
 import { useI18n } from 'vue-i18n';
 
-export default defineComponent({
-  props: {
-    value: {
-      type: [String, Number, BigNumber],
-      required: false,
-      default: '',
-    },
-    smallDecimals: {
-      type: Boolean,
-      default: false,
-    },
-    showDash: {
-      type: Boolean,
-      default: false,
-    },
-    preventZero: {
-      type: Boolean,
-      default: false,
-    },
-    precision: {
-      type: Number,
-      default: 6,
-    },
-  },
-  setup(props) {
-    const maximumFractionDigits = ref(2);
-    const inputValue = computed(() => new BigNumber(props.value));
-    const hasValue = computed(
-      () => !inputValue.value.isNaN() && inputValue.value.isFinite() && inputValue.value.isGreaterThan(0),
-    );
-    const i18n = useI18n({ useScope: 'global' });
-    watch(
-      () => [props.value, props.precision],
-      () => {
-        const bgValue = new BigNumber(props.value);
+interface Props {
+  value?: any; // Using any because BigNumber is not compatible with typescript, before it was [String, Number, BigNumber]
+  smallDecimals?: boolean;
+  showDash?: boolean;
+  preventZero?: boolean;
+  precision?: number;
+}
 
-        // This will prevent formatting smaller values like 0.0001 to 0.00
-        if (bgValue.decimalPlaces(2).isZero()) {
-          maximumFractionDigits.value = props.precision;
-        } else {
-          maximumFractionDigits.value = 2;
-        }
-      },
-      { immediate: true },
-    );
-
-    return { hasValue, maximumFractionDigits, inputValue, i18n };
-  },
+const props = withDefaults(defineProps<Props>(), {
+  value: '',
+  smallDecimals: false,
+  showDash: false,
+  preventZero: false,
+  precision: 6,
 });
+
+const maximumFractionDigits = ref(2);
+
+const inputValue = computed(() => new BigNumber(props.value));
+const hasValue = computed(
+  () => !inputValue.value.isNaN() && inputValue.value.isFinite() && inputValue.value.isGreaterThan(0),
+);
+const i18n = useI18n({ useScope: 'global' });
+
+watch(
+  () => [props.value, props.precision],
+  () => {
+    const bgValue = new BigNumber(props.value);
+
+    // This will prevent formatting smaller values like 0.0001 to 0.00
+    if (bgValue.decimalPlaces(2).isZero()) {
+      maximumFractionDigits.value = props.precision;
+    } else {
+      maximumFractionDigits.value = 2;
+    }
+  },
+  { immediate: true },
+);
 </script>
