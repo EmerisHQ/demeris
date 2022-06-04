cf5fcea6eb4bdc04c7aa203d3bfd32579d0b2712
Jesse Eilers <eitjuh@gmail.com>
2022-06-04 11:48:53 +0200

    script setup for all the other components on the homepage

--- src/components/wizard/previews/PreviewTransfer.vue
+++ src/components/wizard/previews/PreviewTransfer.vue
@@ -101,10 +101,10 @@
   </List>
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
 /* eslint-disable max-lines-per-function */
 import { EmerisBase } from '@emeris/types';
-import { computed, defineComponent, PropType, ref, watch } from 'vue';
+import { computed, ref, watch } from 'vue';
 import { useStore } from 'vuex';
 
 import AmountDisplay from '@/components/common/AmountDisplay.vue';
@@ -117,179 +117,123 @@ import * as Actions from '@/types/actions';
 import { getBaseDenom } from '@/utils/actionHandler';
 import { getOwnAddress } from '@/utils/basic';
 
-export default defineComponent({
-  name: 'PreviewTransfer',
+interface Props {
+  step?: Actions.Step;
+  response?: Actions.Step;
+  fees: Actions.FeeTotals;
+  context?: 'default' | 'widget';
+  bordered?: boolean;
+  isReceipt?: boolean;
+}
 
-  components: {
-    AmountDisplay,
-    Address,
-    ChainName,
-    CircleSymbol,
-    List,
-    ListItem,
-  },
-
-  props: {
-    step: {
-      type: Object as PropType<Actions.Step>,
-      default: undefined,
-    },
-    response: {
-      type: Object as PropType<Actions.Step>,
-      default: undefined,
-    },
-    fees: {
-      type: Object as PropType<Actions.FeeTotals>,
-      required: true,
-    },
-    context: {
-      type: String as PropType<'default' | 'widget'>,
-      default: 'default',
-    },
-    bordered: {
-      type: Boolean,
-      default: true,
-    },
-    isReceipt: {
-      type: Boolean as PropType<boolean>,
-      required: false,
-      default: false,
-    },
-  },
-
-  setup(props) {
-    const store = useStore();
-    const denomName = ref('-');
-
-    const gasPriceLevel = computed(() => store.getters[GlobalGetterTypes.USER.getPreferredGasPriceLevel]);
-
-    const currentStep = computed(() => {
-      return props.response || props.step;
-    });
-    const displayFrom = ref('-');
-    const displayTo = ref('-');
-    const stepType = computed(() => {
-      const description = currentStep.value.description;
-      const descriptionKeyMap = {
-        'Assets Must be transferred to hub first': 'transfer-to-hub',
-        'AssetA must be transferred to hub': 'transfer-to-hub',
-        'AssetB must be transferred to hub': 'transfer-to-hub',
-        'Assets Moved': 'move',
-        'Assets Transferred': 'transfer',
-      };
-
-      return descriptionKeyMap[description] || 'transfer';
-    });
-
-    const hasMultipleTransactions = computed(() => {
-      return currentStep.value.transactions.length > 1;
-    });
-    const includedFees = computed(() => {
-      const included = [];
-      const transactions = (props.step as Actions.Step).transactions;
-      for (const tx of transactions) {
-        if (tx.addFee) {
-          included.push(tx.feeToAdd[0].denom);
-        }
-      }
-      return included;
-    });
-    const transactionInfo = computed(() => {
-      const transactions = currentStep.value.transactions;
-      const firstTransaction = transactions[0] as Record<string, any>;
-      const [lastTransaction] = (transactions.length > 1 ? transactions.slice(-1) : transactions) as Record<
-        string,
-        any
-      >[];
-
-      const isIBC = ['IBCtransferBackward', 'IBCtransferForward'].includes(firstTransaction.type);
-      let fromAmount = firstTransaction.data.amount.amount;
-      if (firstTransaction.addFee) {
-        fromAmount = (
-          parseInt(fromAmount) +
-          parseFloat(firstTransaction.feeToAdd[0].amount[gasPriceLevel.value]) *
-            store.getters[GlobalGetterTypes.USER.getGasLimit]
-        ).toString();
-      }
-      const from = {
-        address: '',
-        amount: fromAmount,
-        chain: firstTransaction.data.chainName || firstTransaction.data.chainName,
-        denom: (firstTransaction.data.amount as EmerisBase.Amount).denom,
-      };
-
-      const to = {
-        amount: firstTransaction.data.amount.amount,
-        address: lastTransaction.data.toAddress,
-        chain:
-          lastTransaction.data.toChain ||
-          lastTransaction.data.destination_chain_name ||
-          lastTransaction.data.chain_name,
-        denom: (lastTransaction.data.amount as EmerisBase.Amount).denom,
-      };
-
-      //from.address = store.getters['demerisAPI/getOwnAddress']({ chain_name: from.chain });
-
-      return {
-        isIBC,
-        from,
-        to,
-      };
-    });
-
-    const formatMultipleChannel = (transaction: Actions.TransferData) => {
-      const getName = (name: string) => store.getters[GlobalGetterTypes.API.getDisplayChain]({ name });
-      // @ts-ignore
-      return `Fee ${getName(transaction.data.chainName)} -> ${getName(transaction.data.toChain)}`;
-    };
-
-    const formatChain = (name: string) => {
-      return 'Fees on ' + store.getters[GlobalGetterTypes.API.getDisplayChain]({ name });
-    };
-
-    const truncateAddress = (address: string) => {
-      return `${address.substring(0, address.indexOf('1') + 6)}…${address.substring(
-        address.length - 3,
-        address.length,
-      )}`;
-    };
-
-    watch(
-      transactionInfo,
-      async (detail) => {
-        if (!detail.from.address) {
-          displayFrom.value = await getOwnAddress({ chain_name: detail.from.chain });
-        }
-        if (detail.to.address) {
-          displayTo.value = detail.to.address;
-        }
-        if (detail.to.chain && !detail.to.address) {
-          displayTo.value = await getOwnAddress({ chain_name: detail.to.chain });
-        }
-        if (detail.isIBC) {
-          denomName.value = await getBaseDenom(detail.from.denom, detail.from.chain);
-        } else {
-          denomName.value = detail.from.denom;
-        }
-      },
-      { immediate: true },
-    );
-
-    return {
-      denomName,
-      stepType,
-      currentStep,
-      formatChain,
-      truncateAddress,
-      transactionInfo,
-      hasMultipleTransactions,
-      formatMultipleChannel,
-      includedFees,
-      displayFrom,
-      displayTo,
-    };
-  },
+const props = withDefaults(defineProps<Props>(), {
+  step: undefined,
+  response: undefined,
+  context: 'default',
+  bordered: true,
+  isReceipt: false,
 });
+
+const store = useStore();
+const denomName = ref('-');
+
+const gasPriceLevel = computed(() => store.getters[GlobalGetterTypes.USER.getPreferredGasPriceLevel]);
+
+const currentStep = computed(() => {
+  return props.response || props.step;
+});
+const displayFrom = ref('-');
+const displayTo = ref('-');
+const stepType = computed(() => {
+  const description = currentStep.value.description;
+  const descriptionKeyMap = {
+    'Assets Must be transferred to hub first': 'transfer-to-hub',
+    'AssetA must be transferred to hub': 'transfer-to-hub',
+    'AssetB must be transferred to hub': 'transfer-to-hub',
+    'Assets Moved': 'move',
+    'Assets Transferred': 'transfer',
+  };
+
+  return descriptionKeyMap[description] || 'transfer';
+});
+
+const hasMultipleTransactions = computed(() => {
+  return currentStep.value.transactions.length > 1;
+});
+const includedFees = computed(() => {
+  const included = [];
+  const transactions = (props.step as Actions.Step).transactions;
+  for (const tx of transactions) {
+    if (tx.addFee) {
+      included.push(tx.feeToAdd[0].denom);
+    }
+  }
+  return included;
+});
+const transactionInfo = computed(() => {
+  const transactions = currentStep.value.transactions;
+  const firstTransaction = transactions[0] as Record<string, any>;
+  const [lastTransaction] = (transactions.length > 1 ? transactions.slice(-1) : transactions) as Record<string, any>[];
+
+  const isIBC = ['IBCtransferBackward', 'IBCtransferForward'].includes(firstTransaction.type);
+  let fromAmount = firstTransaction.data.amount.amount;
+  if (firstTransaction.addFee) {
+    fromAmount = (
+      parseInt(fromAmount) +
+      parseFloat(firstTransaction.feeToAdd[0].amount[gasPriceLevel.value]) *
+        store.getters[GlobalGetterTypes.USER.getGasLimit]
+    ).toString();
+  }
+  const from = {
+    address: '',
+    amount: fromAmount,
+    chain: firstTransaction.data.chainName || firstTransaction.data.chainName,
+    denom: (firstTransaction.data.amount as EmerisBase.Amount).denom,
+  };
+
+  const to = {
+    amount: firstTransaction.data.amount.amount,
+    address: lastTransaction.data.toAddress,
+    chain:
+      lastTransaction.data.toChain || lastTransaction.data.destination_chain_name || lastTransaction.data.chain_name,
+    denom: (lastTransaction.data.amount as EmerisBase.Amount).denom,
+  };
+
+  return {
+    isIBC,
+    from,
+    to,
+  };
+});
+
+const formatChain = (name: string) => {
+  return 'Fees on ' + store.getters[GlobalGetterTypes.API.getDisplayChain]({ name });
+};
+
+const truncateAddress = (address: string) => {
+  return `${address.substring(0, address.indexOf('1') + 6)}…${address.substring(address.length - 3, address.length)}`;
+};
+
+watch(
+  transactionInfo,
+  async (detail) => {
+    if (!detail.from.address) {
+      displayFrom.value = await getOwnAddress({ chain_name: detail.from.chain });
+    }
+    if (detail.to.address) {
+      displayTo.value = detail.to.address;
+    }
+    if (detail.to.chain && !detail.to.address) {
+      displayTo.value = await getOwnAddress({ chain_name: detail.to.chain });
+    }
+    if (detail.isIBC) {
+      denomName.value = await getBaseDenom(detail.from.denom, detail.from.chain);
+    } else {
+      denomName.value = detail.from.denom;
+    }
+  },
+  { immediate: true },
+);
 </script>
 
 <style lang="scss" scoped></style>
