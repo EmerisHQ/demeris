594dec3cd150c0098c7b9822d7c2fc0dcc3c9295
Joon <thedigitalimg@gmail.com>
2022-06-10 16:52:58 +0900

    feat: add osmosis has halted message when offline (#1850)
    * feat: add osmosis has halted message when offline
    
    * fix: adjust margin

--- src/components/swap/Swap.vue
+++ src/components/swap/Swap.vue
@@ -1,6 +1,7 @@
 <template>
   <FeatureRunningConditional name="SWAP_WIDGET_DISABLED">
     <template #deactivated>
+      <SwapOsmosisDown />
       <FeatureRunningConditional name="DEX_AGG">
         <template #deactivated>
           <LiquiditySwap :default-asset="nativeAsset" />
@@ -19,6 +20,7 @@ import { defineProps } from 'vue';
 import FeatureRunningConditional from '@/components/common/FeatureRunningConditional.vue';
 import LiquiditySwap from '@/components/liquidity/Swap.vue';
 import SwapDisabled from '@/components/swap/SwapDisabled.vue';
+import SwapOsmosisDown from '@/components/swap/SwapOsmosisDown.vue';
 import SwapMultiDex from '@/features/swap/SwapMultiDex.vue';
 
 interface SwapProps {
