ae7c286407357d8854493f597600f9b17c565fac
fl-y <thedigitalimg@gmail.com>
2022-06-10 13:05:38 +0900

    fix: adjust margin

--- src/components/swap/SwapOsmosisDown.vue
+++ src/components/swap/SwapOsmosisDown.vue
@@ -1,7 +1,7 @@
 <template>
   <section v-if="osmosisDown" class="w-full rounded-xl h-fit bg-fg p-6 flex flex-col justify-between mb-5">
     <div>
-      <h4 class="text-0 font-bold mb-3.5">Osmosis chain is halted</h4>
+      <h4 class="text-0 font-bold mb-2.5">Osmosis chain is halted</h4>
       <p class="text-muted -text-1 leading-normal">
         The Osmosis chain has been halted due to technical issues. The swap widget which is powered by the Osmosis DEX
         will be unavailable during this time. Your balances on the Osmosis chain might be inaccurate.
